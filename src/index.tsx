93761ae8bc4c771389f0142ee844992ea2d119cb
999hyeon <dandan9509@gmail.com>
2019-12-14 02:49:55 +0900

    build: storybook(ts) 셋팅완료 (#5)
    #4

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled from 'styled-components';
-
-const RedApp = styled.div`
-  background-color: red;
-`;
-
-const App: React.FC = () => {
-  return(
-    <RedApp>Hello React!</RedApp>
-  );
-};
+import App from './App';
 
 ReactDOM.render(
   <App />,
