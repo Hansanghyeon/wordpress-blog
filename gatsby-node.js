0f1c707a026028f3be4dedb7ecffb7402d81012a
999hyeon <dandan9509@gmail.com>
2019-12-27 17:44:02 +0900

    feat: tags 제작 완료

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
+const _ = require('lodash');
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
-  const blogPost = path.resolve('./src/templates/blog-post.tsx');
+  const blogPostTemplate = path.resolve('./src/templates/blog-post.tsx');
+  const tagTemplate = path.resolve('./src/templates/tags.tsx');
   const result = await graphql(
     `
       {
-        allMarkdownRemark(
+        postsRemark: allMarkdownRemark(
           sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
+          limit: 2000
         ) {
           edges {
             node {
@@ -23,6 +25,11 @@ exports.createPages = async ({ graphql, actions }) => {
             }
           }
         }
+        tagsGroup: allMarkdownRemark(limit: 2000) {
+          group(field: frontmatter___tags) {
+            fieldValue
+          }
+        }
       }
     `,
   );
@@ -32,7 +39,7 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges;
+  const posts = result.data.postsRemark.edges;
 
   posts.forEach((post, index) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node;
@@ -40,11 +47,23 @@ exports.createPages = async ({ graphql, actions }) => {
 
     createPage({
       path: post.node.fields.slug,
-      component: blogPost,
+      component: blogPostTemplate,
       context: {
         slug: post.node.fields.slug,
         previous,
         next,
+      },
+    });
+  });
+
+  // Extract tag data from query
+  const tags = result.data.tagsGroup.group;
+  tags.forEach((tag) => {
+    createPage({
+      path: `/tags/${_.kebabCase(tag.fieldValue)}`,
+      component: tagTemplate,
+      context: {
+        tag: tag.fieldValue,
       },
     });
   });
