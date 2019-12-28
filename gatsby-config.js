f5e8a1a42729e189b628dd39a4f39dad12349929
999hyeon <dandan9509@gmail.com>
2019-12-28 17:50:23 +0900

    Feature/#29 thumbnail (#30)
    - thumbnail 추가 쿼리
    - md -> mdx로 변경
    mdx로 변경하면서 md 파일안에서 컴포넌트를 가져와서 사용할 수 있으므로 정적인 포스트를 좀더 커스텀하게 제작할 수 있다.

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,33 +23,6 @@ module.exports = {
         name: 'assets',
       },
     },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-prismjs',
-            options: {
-              showLineNumbers: true,
-            },
-          },
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-        ],
-      },
-    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
@@ -100,6 +73,33 @@ module.exports = {
         classNameLight: 'light-mode',
         storageKey: 'darkMode',
         minify: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1.0725rem',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              showLineNumbers: true,
+            },
+          },
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
       },
     },
   ],
