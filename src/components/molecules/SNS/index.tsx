f7b7bd4442bffd6ab0ceeeefc6eb5bf7f8cd8eb4
999hyeon <dandan9509@gmail.com>
2020-01-01 18:16:34 +0900

    feat: "GNB" 제작완료

---
+++ src/components/molecules/SNS/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Github from '@src/components/atoms/icons/github';
+// import styled from 'styled-components';
+
+const SnsIcon = () => (
+  <div>
+    <Github />
+  </div>
+);
+
+export default SnsIcon;
---
+++ src/utils/colorTheme.tsx
@@ -0,0 +1,11 @@
+const colorTheme = {
+  darkMode: [
+    '#292929',
+    '#303437',
+  ],
+  lightMode: [
+    '#fff',
+  ],
+};
+
+export default colorTheme;
