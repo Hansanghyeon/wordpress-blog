c9de79263829d567d8c44ca920ad075f9010a0d9
999hyeon <dandan9509@gmail.com>
2020-01-04 17:45:43 +0900

    feat: darkMode state 사용 변경 (#47)
    #43 
    
    이전보다 다크모드를 사용하기 좋은 방법으로 변경
    `import useDarkMode from 'use-dark-mode'` 남발을 제거하였음
    styled-components themeprovider 사용법을 정확히? 알게된것같다.

--- src/components/atoms/DarkModeToggle/index.tsx
+++ src/components/atoms/DarkModeToggle/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import useDarkMode from 'use-dark-mode';
 import styled from 'styled-components';
-import { darkModeType } from '@src/utils/interface';
 
-const Toggle = styled.div<darkModeType>`
+const Toggle = styled.div`
   cursor: pointer;
   display: inline-block;
   position: relative;
   width: 90px;
   height: 50px;
-  background-color: ${(props):string => (props.darkMode ? '#3C4145' : '#83D8FF')};
+  background-color: ${(props) => (props.theme.darkMode ? '#3C4145' : '#83D8FF')};
   border-radius: calc(90px - 6px);
   transition: background-color 200ms cubic-bezier(0.445, 0.05, 0.55, 0.95);
 `;
-const ToggleHandler = styled.div<darkModeType>`
+const ToggleHandler = styled.div`
   display: inline-block;
   position: relative;
   z-index: 1;
@@ -21,11 +20,11 @@ const ToggleHandler = styled.div<darkModeType>`
   left: 3px;
   width: calc(50px - 6px);
   height: calc(50px - 6px);
-  background-color: ${(props):string => (props.darkMode ? '#FFE5B5' : '#FFCF96')} ;
+  background-color: ${(props) => (props.theme.darkMode ? '#FFE5B5' : '#FFCF96')} ;
   border-radius: 50px;
   box-shadow: 0 2px 6px rgba(0,0,0,.3);
   transition: all 400ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
-  transform:  ${(props):string => (props.darkMode ? 'translate3d(40px, 0, 0) rotate(0)' : 'rotate(-45deg)')};
+  transform:  ${(props) => (props.theme.darkMode ? 'translate3d(40px, 0, 0) rotate(0)' : 'rotate(-45deg)')};
 `;
 
 const Crater = styled.div`
@@ -35,26 +34,26 @@ const Crater = styled.div`
   transition: opacity 200ms ease-in-out;
   border-radius: 100%;
 `;
-const Crater1 = styled(Crater)<darkModeType>`
+const Crater1 = styled(Crater)`
   top: 18px;
   left: 10px;
   width: 4px;
   height: 4px;
-  opacity: ${(props):number => (props.darkMode ? 1 : 0)};
+  opacity: ${(props) => (props.theme.darkMode ? 1 : 0)};
 `;
-const Crater2 = styled(Crater)<darkModeType>`
+const Crater2 = styled(Crater)`
   top: 28px;
   left: 22px;
   width: 6px;
   height: 6px;
-  opacity: ${(props):number => (props.darkMode ? 1 : 0)};
+  opacity: ${(props) => (props.theme.darkMode ? 1 : 0)};
 `;
-const Crater3 = styled(Crater)<darkModeType>`
+const Crater3 = styled(Crater)`
   top: 10px;
   left: 25px;
   width: 8px;
   height: 8px;
-  opacity: ${(props):number => (props.darkMode ? 1 : 0)};
+  opacity: ${(props) => (props.theme.darkMode ? 1 : 0)};
 `;
 
 const Star = styled.div`
@@ -63,11 +62,11 @@ const Star = styled.div`
   transition: all 300ms cubic-bezier(0.445, 0.05, 0.55, 0.95);
   border-radius: 50%;
 `;
-const Star1 = styled(Star)<darkModeType>`
+const Star1 = styled(Star)`
   top: 10px;
   left: 35px;
   z-index: 0;
-  ${(props): string => (props.darkMode ? `
+  ${(props) => (props.theme.darkMode ? `
     width: 2px;
     height: 2px;
   ` : `
@@ -75,11 +74,11 @@ const Star1 = styled(Star)<darkModeType>`
     height: 3px;
   `)}
 `;
-const Star2 = styled(Star)<darkModeType>`
+const Star2 = styled(Star)`
   top: 18px;
   left: 28px;
   z-index: 1;
-  ${(props): string => (props.darkMode ? `
+  ${(props) => (props.theme.darkMode ? `
     width: 4px;
     height: 4px;
     transform: translate3d(-5px, 0, 0);
@@ -88,11 +87,11 @@ const Star2 = styled(Star)<darkModeType>`
     height: 3px;
   `)}
 `;
-const Star3 = styled(Star)<darkModeType>`
+const Star3 = styled(Star)`
   top: 27px;
   left: 40px;
   z-index: 0;
-  ${(props): string => (props.darkMode ? `
+  ${(props) => (props.theme.darkMode ? `
     width: 2px;
     height: 2px;
     transform: translate3d(-7px, 0, 0);
@@ -101,14 +100,14 @@ const Star3 = styled(Star)<darkModeType>`
     height: 3px;
   `)}
 `;
-const Star4 = styled(Star)<darkModeType>`
+const Star4 = styled(Star)`
   top: 16px;
   left: 11px;
   z-index: 0;
   width: 2px;
   height: 2px;
   transform: translate3d(3px,0,0);
-  ${(props): string => (props.darkMode ? `
+  ${(props) => (props.theme.darkMode ? `
     opacity: 1;
     transform: translate3d(0,0,0);
     transition: all 300ms 200ms cubic-bezier(0.445, 0.05, 0.55, 0.95);
@@ -117,14 +116,14 @@ const Star4 = styled(Star)<darkModeType>`
     transition: all 300ms 0 cubic-bezier(0.445, 0.05, 0.55, 0.95);
   `)}
 `;
-const Star5 = styled(Star)<darkModeType>`
+const Star5 = styled(Star)`
   top: 32px;
   left: 17px;
   z-index: 0;
   width: 3px;
   height: 3px;
   transform: translate3d(3px,0,0);
-  ${(props): string => (props.darkMode ? `
+  ${(props) => (props.theme.darkMode ? `
     opacity: 1;
     transform: translate3d(0,0,0);
     transition: all 300ms 300ms cubic-bezier(0.445, 0.05, 0.55, 0.95);
@@ -133,14 +132,14 @@ const Star5 = styled(Star)<darkModeType>`
     transition: all 300ms 0 cubic-bezier(0.445, 0.05, 0.55, 0.95);
   `)}
 `;
-const Star6 = styled(Star)<darkModeType>`
+const Star6 = styled(Star)`
   top: 36px;
   left: 28px;
   z-index: 0;
   width: 2px;
   height: 2px;
   transform: translate3d(3px,0,0);
-  ${(props): string => (props.darkMode ? `
+  ${(props) => (props.theme.darkMode ? `
     opacity: 1;
     transform: translate3d(0,0,0);
     transition: all 300ms 400ms cubic-bezier(0.445, 0.05, 0.55, 0.95);
@@ -154,18 +153,18 @@ const DarkModeToggle = () => {
   const darkMode = useDarkMode();
   return (
     <div>
-      <Toggle darkMode={darkMode.value} onClick={darkMode.toggle}>
-        <ToggleHandler darkMode={darkMode.value}>
-          <Crater1 darkMode={darkMode.value} />
-          <Crater2 darkMode={darkMode.value} />
-          <Crater3 darkMode={darkMode.value} />
+      <Toggle onClick={darkMode.toggle}>
+        <ToggleHandler>
+          <Crater1 />
+          <Crater2 />
+          <Crater3 />
         </ToggleHandler>
-        <Star1 darkMode={darkMode.value} />
-        <Star2 darkMode={darkMode.value} />
-        <Star3 darkMode={darkMode.value} />
-        <Star4 darkMode={darkMode.value} />
-        <Star5 darkMode={darkMode.value} />
-        <Star6 darkMode={darkMode.value} />
+        <Star1 />
+        <Star2 />
+        <Star3 />
+        <Star4 />
+        <Star5 />
+        <Star6 />
       </Toggle>
     </div>
   );
--- src/components/atoms/SNS/github.tsx
+++ src/components/atoms/SNS/github.tsx
@@ -3,21 +3,16 @@ import { Link } from 'gatsby';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { darkModeType } from '@src/utils/interface';
-import useDarkMode from 'use-dark-mode';
 
-const LinkWrap = styled(Link)<darkModeType>`
+const LinkWrap = styled(Link)`
   text-decoration: none;
-  color: ${(props) => (props.darkMode ? '#fff' : '#000')};
+  color: ${(props) => (props.theme.darkMode ? '#fff' : '#000')};
 `;
 
-const Github = () => {
-  const darkMode = useDarkMode();
-  return (
-    <LinkWrap to="https://github.com/Hansanghyeon" darkMode={darkMode.value}>
-      <FontAwesomeIcon icon={faGithub} />
-    </LinkWrap>
-  );
-};
+const Github = () => (
+  <LinkWrap to="https://github.com/Hansanghyeon">
+    <FontAwesomeIcon icon={faGithub} />
+  </LinkWrap>
+);
 
 export default Github;
