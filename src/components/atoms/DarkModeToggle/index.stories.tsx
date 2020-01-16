4615f5d29cebc9a22b539010f492e845af7576fb
999hyeon <dandan9509@gmail.com>
2020-01-16 10:00:15 +0900

    feat: storybook setting

--- src/components/atoms/DarkModeToggle/index.stories.tsx
+++ src/components/atoms/DarkModeToggle/index.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import DayNightBtn from '@atom/DarkModeToggle';
 
 export default {
-  title: 'atoms|/DayNightBtn',
+  title: 'atoms|DayNightBtn',
   component: DayNightBtn,
 };
 export const standard = () => <DayNightBtn />;
