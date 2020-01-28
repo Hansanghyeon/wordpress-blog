082445801987941dd071faffd51074584e9c2e54
999hyeon <dandan9509@gmail.com>
2020-01-28 09:17:26 +0900

    fix: callout props 에러수정

--- src/components/molecules/Callout/index.tsx
+++ src/components/molecules/Callout/index.tsx
@@ -8,6 +8,8 @@ interface StyledProps {
   bgColor?: string;
 }
 
+// NOTE
+// "Callout은 MDX에서 사용하기때문에 렌더링할때 props에서 theme 값을 가져올 수 없다."
 const Wrap = styled(Container)<StyledProps>`
   background-color: ${props =>
     props.bgColor
@@ -16,7 +18,9 @@ const Wrap = styled(Container)<StyledProps>`
             ? darken(0.2, props.bgColor)
             : lighten(0.2, props.bgColor)
         }`
-      : props.theme.mdx.callout};
+      : `${
+          props.theme.isDark ? darken(0.2, '#2c2c2f') : lighten(0.2, '#fff')
+        }`};
   padding: ${rhythm(1 / 2)};
   margin-bottom: ${rhythm(1)};
   border-radius: 4px;
