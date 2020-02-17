e770ab575fab64734b89a3d3fb87d59d0fc67d42
999hyeon <dandan9509@gmail.com>
2020-02-18 08:00:34 +0900

    fix: config update

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: path.join(__dirname, 'tsconfig.json'),
+    ecmaVersion: 11,
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    '@typescript-eslint/eslint-plugin',
+    'import',
+    'prettier',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
