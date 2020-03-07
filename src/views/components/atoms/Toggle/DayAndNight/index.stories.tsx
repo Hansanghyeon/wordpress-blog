c9565f0736d0bec1ee735956c39af8dfce4ecf1e
Hansanghyeon <dandan9509@gmail.com>
2020-03-07 21:37:21 +0900

    fix: remove storybook

--- src/views/components/atoms/Toggle/DayAndNight/index.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DayNightBtn from '@atom/DarkModeToggle';
-
-export default {
-  title: 'atoms|DayNightBtn',
-  component: DayNightBtn,
-};
-export const standard = () => <DayNightBtn />;
--- src/views/components/organisms/GNB/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import GNB from '.';
-
-export default {
-  title: 'organisms|GNB',
-  component: GNB,
-};
-
-export const standard = () => <GNB />;
