3c76ffa0e3672414b3c77ce71bc5d39c86cca89d
Hansanghyeon <dandan9509@gmail.com>
2020-03-15 14:34:19 +0900

    feat: xl 사이즈이상 대응완료 'MSB'

--- src/views/components/organisms/MSB/index.tsx
+++ src/views/components/organisms/MSB/index.tsx
@@ -1,37 +1,50 @@
 import React from 'react';
 import { Container, Row, Col } from 'styled-bootstrap-grid';
 import styled from 'styled-components';
-
+// Components
 import { Logo } from '@atom/Logo';
+import { rhythm } from '@style/typography';
+import CategoriesNav from '@molecule/nav/Categories';
 
-const LogoWrap = styled.div`
-  max-width: 80px;
-`;
-const Wrap = styled.div`
-  padding: 10px;
+const SideBar = styled.div`
+  width: 200px;
+  height: 100%;
   position: fixed;
   left: 0;
-  top: 45px;
-  width: 100px;
-  background-color: ${props => props.theme.color.bg[0]};
+  top: 0;
+  padding: 0 16px;
+  font-family: 'Fira Code';
+`;
+
+const Main = styled.div`
   height: 100%;
+  padding: ${rhythm(1 / 2)} 0;
+`;
+
+const LogoLayout = styled.div`
+  height: 45px;
+  display: flex;
+  width: 100%;
+  padding: ${rhythm(1 / 4)} 16px;
+  svg {
+    width: auto;
+  }
 `;
 
 const MSB = () => (
-  <Wrap>
-    <Container>
-      <Row>
-        <Col col>
-          <LogoWrap>
-            <Logo />
-          </LogoWrap>
-        </Col>
-      </Row>
-      <Row>
-        <Col col>test</Col>
-      </Row>
-    </Container>
-  </Wrap>
+  <SideBar>
+    <LogoLayout>
+      <Logo />
+    </LogoLayout>
+    <Main>
+      <Container>
+        <Row>
+          <Col col>
+            <CategoriesNav />
+          </Col>
+        </Row>
+      </Container>
+    </Main>
+  </SideBar>
 );
-
 export default MSB;
