37270a1a8cc74d0c40766357a70fd0a926deef11
Hansanghyeon <dandan9509@gmail.com>
2020-03-15 13:07:31 +0900

    style: webfont apply

--- src/views/styles/typography.ts
+++ src/views/styles/typography.ts
@@ -3,8 +3,8 @@ import Typography from 'typography';
 const typography = new Typography({
   baseFontSize: '16px',
   baseLineHeight: 2,
-  headerFontFamily: ['Noto Sans KR', 'Noto Sans', 'serif'],
-  bodyFontFamily: ['Noto Sans KR', 'Noto Sans', 'serif'],
+  headerFontFamily: ['Fira Code', 'Noto Sans KR', 'Noto Sans', 'serif'],
+  bodyFontFamily: ['Fira Code', 'Noto Sans KR', 'Noto Sans', 'serif'],
 });
 
 // Hot reload typography in development.
