f3b5dcbd71d91d1d4b92655ecd043de2a1b90a59
Hansanghyeon <dandan9509@gmail.com>
2020-03-15 12:37:36 +0900

    feat: gatsby link

---
+++ src/views/components/atoms/list/NotionStyle/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  display: grid;
+  grid-template-columns: 16px auto;
+  grid-template-rows: 16px;
+  align-items: center;
+  column-gap: 8px;
+  font-size: 14px;
+  height: 16px;
+  color: ${({ theme }) => theme.color.text[1]};
+  margin-bottom: 4px;
+`;
+const ImgWrap = styled.div`
+  width: 16px;
+  height: 100%;
+  overflow: hidden;
+  border-radius: 3px;
+  font-size: 0;
+  img {
+    width: 100%;
+    margin-bottom: 0;
+  }
+`;
+
+interface Props {
+  imgSrc: string;
+  children: React.ReactNode;
+}
+
+const NotionStyleList = ({ imgSrc, children }: Props) => (
+  <Wrap>
+    <ImgWrap>
+      <img src={imgSrc} alt="" />
+    </ImgWrap>
+    <span>{children}</span>
+  </Wrap>
+);
+
+export default NotionStyleList;
