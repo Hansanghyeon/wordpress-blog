18974a23b4d6cd16d0ce5bda50fcaede082923d7
Hansanghyeon <dandan9509@gmail.com>
2020-03-15 14:32:51 +0900

    style: 사이드바 영역 추가

--- src/views/components/templates/layout.tsx
+++ src/views/components/templates/layout.tsx
@@ -3,22 +3,29 @@ import styled, { ThemeProvider } from 'styled-components';
 import { GridThemeProvider } from 'styled-bootstrap-grid';
 import { ThemeManagerContext } from 'gatsby-styled-components-dark-mode';
 
-import gridTheme from '@utile/gridTheme';
-import { rhythm } from '@utile/typography';
+import Grid from '@style/Grid';
+import { rhythm } from '@style/typography';
 import GNB from '@organism/GNB';
 import FNB from '@organism/FNB';
-import DSB from '@organism/DSB';
 import MSB from '@organism/MSB';
 
-import { LightTheme, DarkTheme } from '@style/Theme';
+import DefaultTheme, { DarkTheme } from '@style/themes';
 
-const MainLayout = styled.div`
-  margin-top: 45px;
+const mt = 45;
+const MainWrapper = styled.div`
+  --mt: ${mt}px;
+  margin-top: var(--mt);
+  height: calc(100% - var(--mt));
+  overflow-y: scroll;
 `;
-const MainInnerLayout = styled.div`
-  margin-left: 100px;
-  margin-right: 400px;
-  padding-top: ${rhythm(2)};
+const PageWrap = styled.div`
+  --mt: ${mt}px;
+  display: flex;
+  min-height: calc(100% - var(--mt));
+`;
+const Main = styled.div`
+  width: 100%;
+  padding: ${rhythm(1 / 2)} 0;
 `;
 
 const Layout = ({ children }: any) => {
@@ -26,20 +33,17 @@ const Layout = ({ children }: any) => {
 
   return (
     <>
-      <ThemeProvider theme={themeContext.isDark ? DarkTheme : LightTheme}>
-        <GridThemeProvider gridTheme={gridTheme}>
+      <ThemeProvider theme={themeContext.isDark ? DarkTheme : DefaultTheme}>
+        <GridThemeProvider gridTheme={Grid}>
           <>
             <GNB />
-            <MSB />
-            <DSB />
-            <MainLayout>
-              <MainInnerLayout>
-                <div>
-                  {children}
-                  <FNB />
-                </div>
-              </MainInnerLayout>
-            </MainLayout>
+            <MainWrapper>
+              <MSB />
+              <PageWrap>
+                <Main>{children}</Main>
+              </PageWrap>
+              <FNB />
+            </MainWrapper>
           </>
         </GridThemeProvider>
       </ThemeProvider>
