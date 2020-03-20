f352b0ae6dbe44a2a121ba80f337da8bcc6ab5ba
Hansanghyeon <dandan9509@gmail.com>
2020-03-20 18:04:59 +0900

    feat: 코드 블럭일때만 함수로 블럭 변경

--- src/views/components/templates/post/Wp/index.tsx
+++ src/views/components/templates/post/Wp/index.tsx
@@ -1,20 +1,26 @@
+/* eslint-disable react/no-danger */
 import React from 'react';
 import { graphql } from 'gatsby';
 
 import SEO from '@view/components/seo';
 // components
 import PostTemplate from '@template/post/index';
+import CodeHighlight from '@utile/CodeHighlight';
 
 const WpPostLayout = ({ data }: any) => {
   const { title, excerpt, content } = data.wpgql.post;
   const wpContentArray = content.split('\n\n\n\n');
-
   return (
     <>
       <SEO title={title} description={excerpt} />
       <PostTemplate>
         {wpContentArray.map((block: string) => {
-          return <div dangerouslySetInnerHTML={{ __html: block }} />;
+          let __html = block;
+          if (__html.indexOf('<pre') !== -1) {
+            return <CodeHighlight code={__html} />;
+          }
+          __html = __html.replace('<code>', '<code class="language-text">');
+          return <div dangerouslySetInnerHTML={{ __html: __html }} />;
         })}
       </PostTemplate>
     </>
