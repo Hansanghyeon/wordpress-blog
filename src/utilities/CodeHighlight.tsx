e714b8e6da1a0811a3300799a3be5ead85a7d5c1
999hyeon <dandan9509@gmail.com>
2020-03-21 19:50:24 +0900

    feat: wp post template 코드블럭
    * style: 'PCSC' 토글 버튼 focus 수정
    
    * style: text, bg 배경값 추가
    
    * fix: router path 경로오류 수정
    
    * feat: 코드블록 검색 정규식
    
    * feat: 코드 블럭일때만 함수로 블럭 변경
    
    * fix: build error (#98)

---
+++ src/utilities/CodeHighlight.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/no-danger */
+import React, { useEffect } from 'react';
+import Prism from 'prismjs';
+
+// prettier-ignore
+// eslint-disable-next-line no-useless-escape
+const CodeBlockRegExp = new RegExp(/\<pre.+class\=\".+\">\<code lang\=\"(.+)\" class\=\".+\"\>([\s\S]+)\<\/code\>\<\/pre\>/g);
+
+interface Props {
+  code: string;
+}
+const CodeHighlight = ({ code }: Props) => {
+  const result = CodeBlockRegExp.exec(code);
+  const lang = result && result[1];
+  const codeString = result && result[2];
+  useEffect(() => {
+    setTimeout(() => Prism.highlightAll(), 0);
+  });
+  return (
+    <pre className={`language-${lang}`}>
+      <code className={`language-${lang}`}>{codeString}</code>
+    </pre>
+  );
+};
+
+export default CodeHighlight;
