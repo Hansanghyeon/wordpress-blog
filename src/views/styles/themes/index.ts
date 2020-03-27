67696a07c5f20ea882b49036a9f107707d582b66
Hansanghyeon <dandan9509@gmail.com>
2020-03-27 09:16:34 +0900

    style: 스타일 컬러 변경

--- src/views/styles/themes/index.ts
+++ src/views/styles/themes/index.ts
@@ -3,12 +3,11 @@ import StyleSystem, { DarkStyleSystem } from '@model/StyleSystem';
 const DefaultTheme: StyleSystem = {
   color: {
     primary: '#0f4c81',
-    bg: ['#f6f8ff', '#fff'],
-    text: ['#333', '#000'],
+    bg: ['#fff', '#f6f8ff'],
+    text: ['#333', '#4A4F57', '#868E96'],
     grayscales: {
-      // prefer scheme color
-      dark: ['#232326', '#303437', '#2c2c2f'],
-      light: ['#fff'],
+      dark: ['#12171f', '#161a23', '#181c27', '#1f2330', '#252836', '#303645'],
+      light: ['#ffffff', '#e6ecf8', '#9ba4b7', '#6d7484', '#404655'],
     },
   },
 };
@@ -16,8 +15,8 @@ const DefaultTheme: StyleSystem = {
 export const DarkTheme: DarkStyleSystem = {
   color: {
     ...DefaultTheme.color,
-    bg: ['#232326', '#303437'],
-    text: ['#f2f4f7', '#fff'],
+    bg: ['#303437', '#232326'],
+    text: ['#f2f4f7', '#e6ecf8', '#9ba4b7'],
   },
 };
 
