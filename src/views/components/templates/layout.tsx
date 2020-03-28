e7c8c60d1757ea3f07364e01d34d921bf1d74a6a
Hansanghyeon <dandan9509@gmail.com>
2020-03-28 21:17:53 +0900

    fix: 글로벌스타일 css in js로 변경

--- src/views/components/templates/layout.tsx
+++ src/views/components/templates/layout.tsx
@@ -1,5 +1,9 @@
 import React, { useContext } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import styled, {
+  ThemeProvider,
+  createGlobalStyle,
+  css,
+} from 'styled-components';
 import { GridThemeProvider } from 'styled-bootstrap-grid';
 import { ThemeManagerContext } from 'gatsby-styled-components-dark-mode';
 
@@ -10,9 +14,8 @@ import MSB from '@organism/MSB';
 
 import DefaultTheme, { DarkTheme } from '@style/themes';
 
-const mt = 45;
 const MainWrapper = styled.div`
-  --mt: ${mt}px;
+  --mt: 45px;
   margin-top: var(--mt);
   height: calc(100% - var(--mt));
   overflow-y: scroll;
@@ -25,12 +28,29 @@ const Main = styled.div`
   width: 100%;
 `;
 
+const _GlobalStyle = css`
+  body {
+    background-color: ${({ theme }) => theme.color.bg[1]};
+    color: ${({ theme }) => theme.color.text[0]};
+    a {
+      color: ${({ theme }) => theme.color.text[0]};
+      &:hover {
+        color: ${({ theme }) => theme.color.primary};
+      }
+    }
+  }
+`;
+const GlobalStyle = createGlobalStyle`
+  ${_GlobalStyle}
+`;
+
 const Layout = ({ children }: any) => {
   const themeContext = useContext(ThemeManagerContext);
 
   return (
     <>
       <ThemeProvider theme={themeContext.isDark ? DarkTheme : DefaultTheme}>
+        <GlobalStyle />
         <GridThemeProvider gridTheme={Grid}>
           <>
             <GNB />
