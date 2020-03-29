508d0f6b40d339801d4f72546db418cf28969215
Hansanghyeon <dandan9509@gmail.com>
2020-03-29 15:16:58 +0900

    feat: 공통스타일로 변경후 이전 'SNS' 묶음으로 적용

--- src/views/components/atoms/icons/Dribbble/index.tsx
+++ src/views/components/atoms/icons/Dribbble/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 // Components
 import Link from '@atom/Link';
-import './index.style.scss';
 
 const Dribbble = () => (
-  <Link href="https://dribbble.com/Hansanghyeon" className="social-dribbble">
+  <Link
+    href="https://dribbble.com/Hansanghyeon"
+    className="social social-dribbble"
+  >
     <img
       src="https://cdn.dribbble.com/assets/dribbble-ball-mark-2bd45f09c2fb58dbbfb44766d5d1d07c5a12972d602ef8b32204d28fa3dda554.svg"
       alt=""
