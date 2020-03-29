b447174c3762539856ad93846c42675c6bcdfd8e
999hyeon <dandan9509@gmail.com>
2020-03-29 15:21:41 +0900

    feat: Dribbble, CodeSandbox SNS추가 (#112)
    * feat: 'Dribbble' 아이콘 추가
    
    * feat: codesandbox 아이콘추가
    
    * feat: 공통스타일로 변경후 이전 'SNS' 묶음으로 적용

---
+++ src/views/components/atoms/icons/Dribbble/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// Components
+import Link from '@atom/Link';
+
+const Dribbble = () => (
+  <Link
+    href="https://dribbble.com/Hansanghyeon"
+    className="social social-dribbble"
+  >
+    <img
+      src="https://cdn.dribbble.com/assets/dribbble-ball-mark-2bd45f09c2fb58dbbfb44766d5d1d07c5a12972d602ef8b32204d28fa3dda554.svg"
+      alt=""
+    />
+  </Link>
+);
+
+export default Dribbble;
