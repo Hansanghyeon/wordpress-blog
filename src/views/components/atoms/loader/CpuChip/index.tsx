431154016692e2bbabc219dad7b91159fbea8d5e
Hansanghyeon <dandan9509@gmail.com>
2020-04-06 21:54:02 +0900

    feat: theme 변수 자체 적용

--- src/views/components/atoms/loader/CpuChip/index.tsx
+++ src/views/components/atoms/loader/CpuChip/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import Lottie from 'react-lottie';
 import CpuChip from './lottie.json';
 
+const CpuChipThemeApply = styled.div`
+  width: 100%;
+  height: 100%;
+  svg.cpu-chip path[stroke='rgb(0,0,0)'] {
+    stroke: ${({ theme }) => theme.color.text[1]};
+  }
+  svg.cpu-chip path[stroke='rgb(241,241,241)'] {
+    stroke: ${({ theme }) => theme.color.bg[1]};
+  }
+  svg.cpu-chip path[fill='rgb(0,0,0)'] {
+    fill: ${({ theme }) => theme.color.text[1]};
+  }
+  svg.cpu-chip path[fill='rgb(241,241,241)'] {
+    fill: ${({ theme }) => theme.color.bg[1]};
+  }
+`;
 const lottieOptions = {
   animationData: CpuChip,
   loop: true,
@@ -11,6 +28,9 @@ const lottieOptions = {
   },
 };
 
-const CpuChipLoader = () => <Lottie options={lottieOptions} />;
-
+const CpuChipLoader = () => (
+  <CpuChipThemeApply>
+    <Lottie options={lottieOptions} />
+  </CpuChipThemeApply>
+);
 export default CpuChipLoader;
