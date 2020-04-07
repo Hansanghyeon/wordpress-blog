292864378d6102a70e339b6a7e9d045dccaecabf
Hansanghyeon <dandan9509@gmail.com>
2020-04-07 12:03:29 +0900

    fix: https 요청구분 수정

--- src/views/containers/SeoPreviewCard/index.tsx
+++ src/views/containers/SeoPreviewCard/index.tsx
@@ -5,10 +5,10 @@ import useRequest from '@utile/useRequest';
 
 export default (url: string) => {
   const [response, loading, error] = useRequest(
-    `http://${
+    `${
       process.env.NODE_ENV === 'production'
-        ? 'api.nas.hapas.io'
-        : 'localhost:8080'
+        ? 'https://api.nas.hapas.io'
+        : 'http://localhost:8080'
     }/seo/?url=${encodeURI(url)}`,
   );
   if (loading) return <SeoPreviewCard loading={loading} reqUrl={url} />;
