40d58976813773c040ef7a705e6a7f3e04ffc5b1
Hansanghyeon <dandan9509@gmail.com>
2020-05-14 19:47:08 +0900

    fix: 오타수정

--- src/views/styles/respondTo.ts
+++ src/views/styles/respondTo.ts
@@ -1,7 +1,7 @@
 import { breakpoints } from './Grid';
 
 const respondTo = {
-  hg: `@media (min-width: ${breakpoints.xl + 1}px)`,
+  hg: `@media (min-width: ${breakpoints.hg + 1}px)`,
   xl: `@media (min-width: ${breakpoints.xl + 1}px)`,
   lg: `@media (min-width: ${breakpoints.lg + 1}px)`,
   md: `@media (min-width: ${breakpoints.md + 1}px)`,
