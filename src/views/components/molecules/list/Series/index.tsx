051646ccf39e70f8659aeb958cf7cce168a30c31
Hansanghyeon <dandan9509@gmail.com>
2020-05-17 20:20:25 +0900

    feat: 시리즈 컴포넌트 1.0.0
    docs: 시리즈 컴포넌트 1.0.0

---
+++ src/views/components/molecules/list/Series/index.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCaretDown,
+  faCaretUp,
+  faChevronRight,
+  faChevronLeft,
+} from '@fortawesome/free-solid-svg-icons';
+import Link from '@atom/Link';
+import {
+  Img,
+  Wrap,
+  Header,
+  Title,
+  Footer,
+  Controller,
+  Body,
+  Ol,
+  Li,
+  MoreBtn,
+  ArrowBtn,
+  CurrentLi,
+} from './index.style';
+import { SeriesListType, accPostType } from './index.type';
+
+const SeriesList: React.FC<SeriesListType> = ({ data }: SeriesListType) => {
+  const { query, currentPostId } = data;
+  const [toggle, setToggle] = useState(true);
+  const _handleClick = () => {
+    setToggle(!toggle);
+  };
+  const prevNextPost = query.posts.edges.reduce(
+    (acc: accPostType, cur, idx, arr) => {
+      const { node } = cur;
+      if (arr.length <= 1) return acc;
+      if (node.id === currentPostId) {
+        if (idx === 0) {
+          acc.next = arr[idx + 1];
+          return acc;
+        }
+        if (idx >= 1) {
+          acc.prev = arr[idx - 1];
+          acc.next = arr[idx + 1];
+          return acc;
+        }
+      }
+      return acc;
+    },
+    { prev: null, next: null },
+  );
+  return (
+    <Wrap>
+      <Header toggle={toggle}>
+        <Title>{query.name}</Title>
+        <Img
+          src="https://wp.hapas.io/wp-content/uploads/icons/book.svg"
+          alt="Series"
+        />
+      </Header>
+      <Body toggle={toggle}>
+        <Ol>
+          {query.posts.edges.map(({ node }) => {
+            if (node.id === currentPostId) {
+              return (
+                <CurrentLi key={`${currentPostId}-${node.id}`}>
+                  {node.title}
+                </CurrentLi>
+              );
+            }
+            return (
+              <Li key={`${currentPostId}-${node.id}`}>
+                <Link to={`/${node.slug}`}>{node.title}</Link>
+              </Li>
+            );
+          })}
+        </Ol>
+      </Body>
+      <Footer>
+        <MoreBtn type="button" onClick={_handleClick}>
+          <FontAwesomeIcon icon={toggle ? faCaretDown : faCaretUp} />
+          <span>{toggle ? '목록보기' : '접기'}</span>
+        </MoreBtn>
+        <Controller>
+          {prevNextPost.prev ? (
+            <Link to={`/${prevNextPost.prev.node.slug}`}>
+              <ArrowBtn type="button">
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </ArrowBtn>
+            </Link>
+          ) : (
+            <ArrowBtn type="button" disable>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </ArrowBtn>
+          )}
+          {prevNextPost.next ? (
+            <Link to={`/${prevNextPost.next.node.slug}`}>
+              <ArrowBtn type="button">
+                <FontAwesomeIcon icon={faChevronRight} />
+              </ArrowBtn>
+            </Link>
+          ) : (
+            <ArrowBtn type="button" disable>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </ArrowBtn>
+          )}
+        </Controller>
+      </Footer>
+    </Wrap>
+  );
+};
+
+export default SeriesList;
