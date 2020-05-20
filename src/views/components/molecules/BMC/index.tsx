ca805c2af3f5d58310121e2e9cc8dfb4cb645736
Hansanghyeon <dandan9509@gmail.com>
2020-05-20 21:40:40 +0900

    feat: bmc 버튼bio 섹션에 추가

---
+++ src/views/components/molecules/BMC/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Coffee, Title, Button } from './index.style';
+
+const BMC = () => {
+  return (
+    <Button href="https://www.buymeacoffee.com/r1N7GR5" target="_blank">
+      <Coffee src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" />
+      <Title>Buy me a coffee</Title>
+    </Button>
+  );
+};
+
+export default BMC;
