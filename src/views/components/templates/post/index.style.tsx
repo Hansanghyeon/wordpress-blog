dece7a44e384985bbf97e8a2379950f20211964a
Hansanghyeon <dandan9509@gmail.com>
2020-05-27 23:04:12 +0900

    refactor: 스타일코드 모듈화 분리

---
+++ src/views/components/templates/post/index.style.tsx
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+import { rhythm } from '@style/typography';
+import { Container, Row as _Row, Col, media } from 'styled-bootstrap-grid';
+
+export const CardWrap = styled.div`
+  background: ${({ theme }) => theme.color.bg[0]};
+  border-radius: 3px;
+  padding: ${rhythm(1 / 2)};
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.1);
+  ${media.md`
+    padding: ${rhythm(1)};
+  `}
+`;
+
+export const FeaturedImg = styled(Col)<{ bgImg?: string }>`
+  background-image: url(${({ bgImg }) => bgImg});
+  background-size: cover;
+  background-position: center;
+  padding-top: 54%;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+  background-color: ${({ theme }) => theme.color.bg[0]};
+  ${media.lg`
+    padding-top: calc(54% / 2);
+  `}
+`;
+
+export const StyledContainer = styled(Container)`
+  margin-bottom: ${rhythm(2)};
+`;
+
+export const PostData = styled(Col)`
+  padding-top: ${rhythm(1 / 2)};
+  padding-bottom: ${rhythm(1 / 2)};
+  margin-top: ${rhythm(1)};
+  order: 2;
+  ${media.lg`
+    order: 0;
+    justify-content: flex-end;
+    flex-direction: column;
+    display: flex;
+  `}
+`;
+
+export const EtcWrap = styled.div`
+  display: flex;
+  margin-top: ${rhythm(1 / 2)};
+`;
+
+/**
+ * @Row
+ * */
+const MainRow = styled(_Row)`
+  margin-bottom: ${rhythm(1)};
+  margin-top: ${rhythm(1)};
+  .fc-dim,
+  .fc-dim * {
+    color: ${({ theme }) => theme.color.text[3]};
+  }
+  blockquote {
+    border-left: 4px solid ${({ theme }) => theme.color.bg[1]};
+  }
+  .fc-highlight {
+    color: ${({ theme }) => theme.color.primary};
+  }
+  .how-block {
+    margin-top: 0;
+  }
+  hr {
+    border-bottom-color: ${({ theme }) => theme.color.text[3]};
+  }
+  > ${Col} {
+    position: unset;
+  }
+  a {
+    font-size: 0.9em;
+    text-decoration-line: underline;
+    text-decoration-color: ${({ theme }) => theme.color.primary};
+  }
+`;
+const BioRow = styled(_Row)`
+  padding: 0 ${rhythm(1)};
+`;
+const CommentsRow = styled(_Row)`
+  margin-bottom: ${rhythm(2)};
+`;
+const FeaturedImgRow = styled(_Row)`
+  margin-left: 0;
+  margin-right: 0;
+  overflow: hidden;
+`;
+
+export const Row = {
+  Main: MainRow,
+  Bio: BioRow,
+  FeaturedImg: FeaturedImgRow,
+  Comment: CommentsRow,
+};
--- src/views/components/templates/post/index.tsx
+++ src/views/components/templates/post/index.tsx
@@ -1,117 +1,30 @@
 import React from 'react';
-import styled from 'styled-components';
-import {
-  GridThemeProvider,
-  Container,
-  Row,
-  Col,
-  media,
-} from 'styled-bootstrap-grid';
+import { GridThemeProvider, Col } from 'styled-bootstrap-grid';
 import Grid from '@style/Grid';
 import Bio from '@view/components/bio';
-import { rhythm } from '@style/typography';
 import moment from 'moment';
 // Utils
 // components
 import CCBox from '@atom/icons/CCBox';
 import PostTitleText from '@atom/text/PostTitle';
 import Utterance from '#/Utterances';
+import {
+  CardWrap,
+  FeaturedImg,
+  StyledContainer,
+  PostData,
+  EtcWrap,
+  Row,
+} from './index.style';
 
-const CardWrap = styled.div`
-  background: ${({ theme }) => theme.color.bg[0]};
-  border-radius: 3px;
-  padding: ${rhythm(1 / 2)};
-  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.1);
-  ${media.md`
-    padding: ${rhythm(1)};
-  `}
-`;
-
-const CommentsRow = styled(Row)`
-  margin-bottom: ${rhythm(2)};
-`;
-
-const FeaturedImgRow = styled(Row)`
-  margin-left: 0;
-  margin-right: 0;
-  overflow: hidden;
-`;
-
-interface bgImg {
-  bgImg?: string;
-}
-const FeaturedImg = styled(Col)<bgImg>`
-  background-image: url(${({ bgImg }) => bgImg});
-  background-size: cover;
-  background-position: center;
-  padding-top: 54%;
-  border-bottom-left-radius: 4px;
-  border-bottom-right-radius: 4px;
-  background-color: ${({ theme }) => theme.color.bg[0]};
-  ${media.lg`
-    padding-top: calc(54% / 2);
-  `}
-`;
-
-const StyledRow = styled(Row)`
-  margin-bottom: ${rhythm(1)};
-  margin-top: ${rhythm(1)};
-  .fc-dim,
-  .fc-dim * {
-    color: ${({ theme }) => theme.color.text[3]};
-  }
-  blockquote {
-    border-left: 4px solid ${({ theme }) => theme.color.bg[1]};
-  }
-  .fc-highlight {
-    color: ${({ theme }) => theme.color.primary};
-  }
-  .how-block {
-    margin-top: 0;
-  }
-  hr {
-    border-bottom-color: ${({ theme }) => theme.color.text[3]};
-  }
-  > ${Col} {
-    position: unset;
-  }
-`;
-
-const BioRow = styled(StyledRow)`
-  padding: 0 ${rhythm(1)};
-`;
-
-const StyledContainer = styled(Container)`
-  margin-bottom: ${rhythm(2)};
-`;
-
-const PostData = styled(Col)`
-  padding-top: ${rhythm(1 / 2)};
-  padding-bottom: ${rhythm(1 / 2)};
-  margin-top: ${rhythm(1)};
-  order: 2;
-  ${media.lg`
-    order: 0;
-    justify-content: flex-end;
-    flex-direction: column;
-    display: flex;
-  `}
-`;
-
-const EtcWrap = styled.div`
-  display: flex;
-  margin-top: ${rhythm(1 / 2)};
-`;
-
-interface header {
-  title: string;
-  date: string;
-}
-interface props {
+type props = {
   children: React.ReactNode;
   imgSrc?: string;
-  header: header;
-}
+  header: {
+    title: string;
+    date: string;
+  };
+};
 
 const PostTemplate = ({ children, imgSrc, header }: props) => {
   const _Grid = {
@@ -126,7 +39,7 @@ const PostTemplate = ({ children, imgSrc, header }: props) => {
     <>
       <GridThemeProvider gridTheme={_Grid}>
         <StyledContainer>
-          <FeaturedImgRow>
+          <Row.FeaturedImg>
             <PostData col={12} lg={imgSrc ? 6 : 12}>
               <PostTitleText>{header.title}</PostTitleText>
               <span>{moment(header.date).format('YYYY년 MM월 DD일')}</span>
@@ -135,23 +48,23 @@ const PostTemplate = ({ children, imgSrc, header }: props) => {
               </EtcWrap>
             </PostData>
             {imgSrc && <FeaturedImg col={12} lg={6} bgImg={imgSrc} />}
-          </FeaturedImgRow>
-          <StyledRow>
+          </Row.FeaturedImg>
+          <Row.Main>
             <Col col>
               <CardWrap className="prism-previewer">{children}</CardWrap>
             </Col>
-          </StyledRow>
-          <BioRow>
+          </Row.Main>
+          <Row.Bio>
             <Col col>
               <Bio />
             </Col>
-          </BioRow>
+          </Row.Bio>
           {process.env.NODE_ENV === 'production' && (
-            <CommentsRow>
+            <Row.Comment>
               <Col col>
                 <Utterance />
               </Col>
-            </CommentsRow>
+            </Row.Comment>
           )}
         </StyledContainer>
       </GridThemeProvider>
