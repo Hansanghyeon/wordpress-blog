3cbd3218e1ea8ce5c442c066787377dbd89cac1e
999hyeon <dandan9509@gmail.com>
2020-05-28 18:14:06 +0900

    feat: download button (#168)

--- src/views/components/molecules/SyntaxHighlighter/style.tsx
+++ src/views/components/molecules/SyntaxHighlighter/style.tsx
@@ -5,6 +5,7 @@ export const Header = styled.div`
   justify-content: space-between;
   transform: translateY(0.8rem);
   font-size: 0.85rem;
+  margin-top: -0.8rem;
 `;
 export const FileName = styled.div`
   padding: 0.2em 1em;
