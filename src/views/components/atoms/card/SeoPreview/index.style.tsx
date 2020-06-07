bdcc867c0c769adefbd81f08f5dab70e1e2b65b3
Hansanghyeon <dandan9509@gmail.com>
2020-06-07 10:52:46 +0900

    refactor: scss 제거 styled-component로 모두 통합

---
+++ src/views/components/atoms/card/SeoPreview/index.style.tsx
@@ -0,0 +1,110 @@
+import styled from 'styled-components';
+import { Row as _Row, Col as _Col, media } from 'styled-bootstrap-grid';
+
+import Link from '@atom/Link';
+
+type styleProps = {
+  bg?: string;
+};
+export const Header = styled.div`
+  background-color: ${({ theme }) => theme.color.bg[0]};
+
+  display: flex;
+  align-items: center;
+  font-size: 10px;
+  position: absolute;
+  top: 2px;
+  left: 0;
+  border-radius: 2px;
+  padding-right: 2px;
+  box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.1);
+  transition: all 0.45s;
+  will-change: background-color, color;
+`;
+export const Favicon = styled.div`
+  border-radius: 2px;
+  overflow: hidden;
+  line-height: 0;
+  width: 16px;
+  height: 16px;
+  margin-right: 4px;
+  img {
+    width: 100%;
+  }
+`;
+export const Url = styled.div`
+  max-width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+export const Description = styled.div`
+  margin-bottom: 4px;
+  ${media.md`
+    max-height: 3.8rem;
+    overflow: hidden;
+  `};
+`;
+export const Col = {
+  def: styled(_Col)``,
+  Image: styled(_Col)<styleProps>`
+    background-image: ${({ bg }) => `url(${bg})`};
+    display: none;
+    background-size: cover;
+    background-position: center;
+    ${media.md`
+      display: block;
+    `};
+  `,
+  Content: styled(_Col)`
+    overflow: hidden;
+    font-size: 12px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    ${media.md`
+      height: 120px;
+    `};
+  `,
+};
+export const Row = {
+  def: styled(_Row)``,
+  Body: styled(_Row)`
+    background-color: ${({ theme }) => theme.color.bg[1]};
+    color: ${({ theme }) => theme.color.text[2]};
+    font-size: 14px;
+    border-radius: 4px;
+    box-shadow: inset 0 0 5px 1px rgba(0, 0, 0, 0.1);
+    transition: all 0.45s;
+    will-change: box-shadow, color, border;
+    box-sizing: border-box;
+    border: 1px solid transparent;
+  `,
+};
+export const RootWrap = styled(Link)`
+  display: block;
+  color: inherit;
+  text-decoration: none;
+  padding: 16px 0;
+  position: relative;
+  &:hover {
+    box-shadow: inset 0 0 5px 1px rgba(0, 0, 0, 0);
+  }
+
+  ${_Row} {
+    margin-left: 0;
+    margin-right: 0;
+  }
+  &:hover {
+    ${Header} {
+      background-color: ${({ theme }) => theme.color.primary};
+      color: ${({ theme }) => theme.color.bg[1]};
+    }
+    ${Row.Body} {
+      color: ${({ theme }) => theme.color.text[1]};
+      border: 1px solid ${({ theme }) => theme.color.text[2]};
+    }
+  }
+`;
--- src/views/components/atoms/card/SeoPreview/index.tsx
+++ src/views/components/atoms/card/SeoPreview/index.tsx
@@ -1,89 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Row, Col } from 'styled-bootstrap-grid';
-import './index.style.scss';
 // Components
 import NonFavicon from '@atom/icons/NonFavicon';
 import CpuChipLoader from '@atom/loader/CpuChip';
+import {
+  Col,
+  Row,
+  Header,
+  Favicon,
+  Url,
+  RootWrap,
+  Description,
+} from './index.style';
 
-const Header = styled.div`
-  background-color: ${({ theme }) => theme.color.bg[0]};
-`;
-const Favicon = styled.div``;
-const Body = styled(Row)`
-  background-color: ${({ theme }) => theme.color.bg[1]};
-  color: ${({ theme }) => theme.color.text[2]};
-`;
-const Url = styled.div``;
-const ImageCol = styled(Col)<styleProps>`
-  background-image: ${({ bg }) => `url(${bg})`};
-`;
-const Content = styled(Col)``;
-const RootWrap = styled.a`
-  ${Row} {
-    margin-left: 0;
-    margin-right: 0;
-  }
-  &:hover {
-    ${Header} {
-      background-color: ${({ theme }) => theme.color.primary};
-      color: ${({ theme }) => theme.color.bg[1]};
-    }
-    ${Body} {
-      color: ${({ theme }) => theme.color.text[1]};
-      border: 1px solid ${({ theme }) => theme.color.text[2]};
-    }
-  }
-`;
-
-const Loaded = ({ data, mUrl }: reqData) => {
-  const { title, description, favicon, image } = data;
-  return (
-    <>
-      <Header className="_header">
-        <Favicon className="_favicon">
-          {favicon ? <img src={favicon} alt="" /> : <NonFavicon />}
-        </Favicon>
-        <div>{title}</div>
-      </Header>
-      <Body className="_body">
-        <Content col className="_content">
-          <div className="_description">{description}</div>
-          <Url className="_url">{mUrl}</Url>
-        </Content>
-        {image && <ImageCol col={12} sm={4} bg={image} className="_thumnail" />}
-      </Body>
-    </>
-  );
-};
-const Loading = () => (
-  <>
-    <Header className="_header">
-      <Favicon className="_favicon">
-        <NonFavicon />
-      </Favicon>
-      <div>Loading...</div>
-    </Header>
-    <Body className="_body">
-      <Col col className="_content">
-        <CpuChipLoader />
-      </Col>
-    </Body>
-  </>
-);
-
-const SeoPreviewCard = ({ data, loading, mUrl }: props) => {
-  return (
-    <RootWrap href={mUrl} className={`seoPreview ${loading && 'loading'}`}>
-      {!loading ? <Loaded data={data} mUrl={mUrl} /> : <Loading />}
-    </RootWrap>
-  );
-};
-
-export default SeoPreviewCard;
-interface styleProps {
-  bg?: string;
-}
 interface reqData {
   data?: {
     title: string;
@@ -97,3 +25,51 @@ interface reqData {
 interface props extends reqData {
   loading: boolean;
 }
+
+const Loaded = ({ data, mUrl }: reqData) => {
+  if (data === undefined) return <></>;
+  const { title, description, favicon, image } = data;
+  return (
+    <>
+      <Header>
+        <Favicon>
+          {favicon ? <img src={favicon} alt="" /> : <NonFavicon />}
+        </Favicon>
+        <div>{title}</div>
+      </Header>
+      <Row.Body>
+        <Col.Content col>
+          <Description>{description}</Description>
+          <Url>{mUrl}</Url>
+        </Col.Content>
+        {image && <Col.Image col={12} sm={4} bg={image} />}
+      </Row.Body>
+    </>
+  );
+};
+
+const Loading = () => (
+  <>
+    <Header>
+      <Favicon>
+        <NonFavicon />
+      </Favicon>
+      <div>Loading...</div>
+    </Header>
+    <Row.Body>
+      <Col.def col>
+        <CpuChipLoader />
+      </Col.def>
+    </Row.Body>
+  </>
+);
+
+const SeoPreviewCard = ({ data, loading, mUrl }: props) => {
+  return (
+    <RootWrap href={mUrl}>
+      {!loading ? <Loaded data={data} mUrl={mUrl} /> : <Loading />}
+    </RootWrap>
+  );
+};
+
+export default SeoPreviewCard;
