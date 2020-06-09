5a49bcbfcb83d7ff91026a040b3ba1c63618c800
Hansanghyeon <dandan9509@gmail.com>
2020-06-09 20:29:44 +0900

    feat: 커스텀훅 생성 outside 클릭이벤트

---
+++ src/utilities/useOutsideAlerter/index.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+function useOutsideAlerter({ ref, inFunction }) {
+  /**
+   * Alert if clicked on outside of element
+   */
+  function handleClickOutside(event) {
+    if (ref.current && !ref.current.contains(event.target)) {
+      inFunction();
+    }
+  }
+
+  useEffect(() => {
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  });
+}
+
+export default useOutsideAlerter;
