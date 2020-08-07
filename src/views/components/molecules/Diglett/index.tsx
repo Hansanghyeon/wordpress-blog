216baa8a1da30561eed1330bc5b834f29dcbea64
Hansanghyeon <dandan9509@gmail.com>
2020-08-07 19:19:00 +0900

    feat: diglett 방향설정 추가

--- src/views/components/molecules/Diglett/index.tsx
+++ src/views/components/molecules/Diglett/index.tsx
@@ -1,33 +1,57 @@
 import React, { useState, useEffect } from 'react';
 // component
-import { FnbType } from '@store/Layout/index.type';
 import { DiglettRoot, MoveWrap } from './index.style';
 
-const variansts = {
+const bottom_variansts = {
   show: { translateY: 0 },
   hidden: { translateY: '100%' },
   toggle: {
     translateY: [0, 0, 0, '100%', '100%', 0, 0],
-    borderTopLeftRadius: [0, 0, '20%', '20%', '20%', '20%', 0],
-    borderTopRightRadius: [0, 0, '20%', '20%', '20%', '20%', 0],
+    borderTopLeftRadius: [0, 0, '30px', '30px', '30px', '30px', 0],
+    borderTopRightRadius: [0, 0, '30px', '30px', '30px', '30px', 0],
+  },
+};
+const right_variansts = {
+  show: { translateX: 0 },
+  hidden: { translateX: '-100%' },
+  toggle: {
+    translateX: [0, 0, 0, '-100%', '-100%', 0, 0],
+    borderTopLeftRadius: [0, 0, '30px', '30px', '30px', '30px', 0],
+    borderTopRightRadius: [0, 0, '30px', '30px', '30px', '30px', 0],
   },
 };
 
 type props = {
-  type: FnbType;
-  RenderComponent: ({ type }: { type: FnbType }) => JSX.Element;
+  direction?: string;
+  type: string;
+  RenderComponent: ({ type }: { type: string }) => JSX.Element;
 };
-const Diglett: React.FC<props> = ({ type, RenderComponent }: props) => {
-  const [delayedType, setDelayedType] = useState(FnbType.default);
+const Diglett: React.FC<props> = ({
+  direction,
+  type,
+  RenderComponent,
+}: props) => {
+  const [delayedType, setDelayedType] = useState('');
+  let variansts;
+  switch (direction) {
+    case 'left':
+      variansts = right_variansts;
+      break;
+    default:
+      variansts = bottom_variansts;
+      break;
+  }
+
   useEffect(() => {
     const timeout = setTimeout(() => setDelayedType(type), 400);
     return () => clearTimeout(timeout);
   });
+
   return (
     <DiglettRoot>
       <MoveWrap
         key={type}
-        animate={type === FnbType.hidden ? 'hidden' : 'toggle'}
+        animate={type === 'hidden' ? 'hidden' : 'toggle'}
         variants={variansts}
         transition={{
           duration: 2.5,
