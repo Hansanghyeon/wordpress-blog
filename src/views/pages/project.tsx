1869943adcc01815b1a256dd96c1112ef8068f5f
Hansanghyeon <dandan9509@gmail.com>
2020-08-09 17:25:58 +0900

    feat: 프로젝트 템플릿으로 이전

--- src/views/pages/project.tsx
+++ src/views/pages/project.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 // components
 import Project from '@template/Project';
-import { Container, Row, Col } from './page.style';
 
 const ProjectPage = () => {
-  return (
-    <Container.Project>
-      <Row.Def>
-        <Col.Def>
-          <Project />
-        </Col.Def>
-      </Row.Def>
-    </Container.Project>
-  );
+  return <Project />;
 };
 
 export default ProjectPage;
