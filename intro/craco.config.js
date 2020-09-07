117a6099b4b706659321d76acd15ebe51aaf4197
999hyeon <dandan9509@gmail.com>
2020-09-07 18:03:27 +0900

    feat: intro 마이그레이션 (#3)

---
+++ intro/craco.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const resolve = arg => path.resolve(__dirname, arg);
+
+module.exports = function() {
+  return {
+    babel: {
+      plugins: ['babel-plugin-styled-components'],
+    },
+    webpack: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+    jest: {
+      configure: {
+        moduleNameMapper: {
+          '^@/(.*)$': '<rootDir>/src/$1',
+        },
+      },
+    },
+  };
+};
