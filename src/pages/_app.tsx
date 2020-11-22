df363ec6a20d5ece8c0984a140ae9adaf406f4f9
Hansanghyeon <dandan9509@gmail.com>
2020-11-22 11:08:13 +0900

    :construction: 전역 provider 이외 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,12 @@
-import '../styles/index.css'
-import { AppProps } from 'next/app';
-import { GridThemeProvider } from 'styled-bootstrap-grid';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import Theme from '@style/Themes';
-// components
-import Grid from '@style/Grid';
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    font-family: 'Noto Sans KR', sans-serif;
-  }
-`;
+import "../styles/index.css";
+import { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={Theme}>
-      <GridThemeProvider gridTheme={Grid}>
-        <>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </>
-      </GridThemeProvider>
-    </ThemeProvider>
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
   );
 }
 
