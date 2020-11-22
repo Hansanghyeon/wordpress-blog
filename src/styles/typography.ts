aeba41bdf7d7f5f95af451ec440a4791eb1b5115
Hansanghyeon <dandan9509@gmail.com>
2020-11-22 11:33:26 +0900

    :heavy_plus_sign: typograpy system 모듈추가

---
+++ src/styles/typography.ts
@@ -0,0 +1,16 @@
+import Typography from "typography";
+
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.6,
+  headerFontFamily: ["Nanum Gothic", "Noto Sans", "serif"],
+  bodyFontFamily: ["Noto Sans KR", "Noto Sans", "serif"],
+});
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== "production") {
+  typography.injectStyles();
+}
+
+export default typography;
+export const { rhythm, scale } = typography;
