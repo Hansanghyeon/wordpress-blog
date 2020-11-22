332bc3feb0609fc36be37333f319d233f1c18f68
Hansanghyeon <dandan9509@gmail.com>
2020-11-22 11:05:13 +0900

    :construction: 폰트추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,6 +30,27 @@ class _Document extends Document {
     return (
       <html>
         <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Cookie&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/gh/joungkyun/font-d2coding/d2coding.css"
+          />
         </Head>
         <body>
           <Main />
