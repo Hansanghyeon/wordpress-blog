e9eef931c758cab39983a884a415033d044d3e0a
Hansanghyeon <dandan9509@gmail.com>
2020-11-25 15:55:21 +0900

    :bulb: 메타컴포넌트 폴더이동 해당 파비콘 이미지 변경

--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -1,19 +1,21 @@
-import { GridThemeProvider } from "styled-bootstrap-grid";
-import { ThemeProvider } from "styled-components";
-import useDarkMode from "use-dark-mode";
+import { GridThemeProvider } from 'styled-bootstrap-grid';
+import { ThemeProvider } from 'styled-components';
+import useDarkMode from 'use-dark-mode';
 // style
-import { MainWrapper, Main, GlobalStyle } from "./style";
-import Grid from "@style/Grid";
-import Theme, { DarkTheme } from "@style/Themes";
+import { MainWrapper, Main, GlobalStyle } from './style';
+import Grid from '@style/Grid';
+import Theme, { DarkTheme } from '@style/Themes';
 // state
 // components
 // import GNB from "@organism/GNB";
-import FNB from "@organism/FNB";
+import FNB from '@organism/FNB';
+import Meta from './meta';
 
 const Layout: React.FC = ({ children }) => {
   const darkMode = useDarkMode();
   return (
     <>
+      <Meta />
       <ThemeProvider theme={darkMode.value ? DarkTheme : Theme}>
         <GlobalStyle />
         <GridThemeProvider gridTheme={Grid}>
