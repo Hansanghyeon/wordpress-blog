b6056507e901b7795b64f7dda0330ec533131ea5
Hansanghyeon <999@hyeon.pro>
2021-01-03 17:41:08 +0900

    :sparkles: INBOX라는 타이틀 제거

--- src/components/modules/list/Inbox/index.tsx
+++ src/components/modules/list/Inbox/index.tsx
@@ -5,18 +5,10 @@ import NotionListItem from '@atom/listItem/Notion';
 import Link from 'next/link';
 
 const Wrap = styled.div``;
-const TitleWrap = styled.div`
-  font-size: 18px;
-  padding: 8px 0;
-`;
 
 const InboxList = ({ data }: any) => {
   return (
     <Wrap>
-      <TitleWrap>
-        <span>🗃</span>
-        INBOX
-      </TitleWrap>
       {data.edges.map(({ node }: any) => {
         const { icon } = node._acf_post;
         return (
