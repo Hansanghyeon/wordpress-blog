8335ecec5c94a8c59f69e41365ece9eaaf4f7e81
Hansanghyeon <dandan9509@gmail.com>
2021-01-18 19:01:25 +0900

    acp 메뉴 로컬스토리지 관리

---
+++ Theme/acp-settings/6000e5613f58a.php
@@ -0,0 +1,86 @@
+<?php
+
+return array (
+  'version' => '5.4',
+  'title' => '[개발] 카테고리',
+  'type' => 'wp-taxonomy_dev_category',
+  'id' => '6000e5613f58a',
+  'updated' => 1610671530,
+  'columns' => 
+  array (
+    '6000e5733e2b3' => 
+    array (
+      'type' => 'column-acf_field',
+      'label' => '아이콘',
+      'width' => '80',
+      'width_unit' => 'px',
+      'field' => 'field_5f58611d5426f',
+      'image_size' => 'cpac-custom',
+      'image_size_w' => '60',
+      'image_size_h' => '60',
+      'export' => 'off',
+      'sort' => 'on',
+      'edit' => 'on',
+      'bulk_edit' => 'on',
+      'search' => 'on',
+      'filter' => 'off',
+      'filter_label' => '',
+    ),
+    'name' => 
+    array (
+      'type' => 'name',
+      'label' => '이름',
+      'width' => '',
+      'width_unit' => '%',
+      'export' => 'off',
+      'sort' => 'on',
+      'edit' => 'on',
+      'bulk_edit' => 'on',
+    ),
+    'description' => 
+    array (
+      'type' => 'description',
+      'label' => '설명',
+      'width' => '',
+      'width_unit' => '%',
+      'export' => 'off',
+      'sort' => 'on',
+      'edit' => 'on',
+      'bulk_edit' => 'on',
+    ),
+    'slug' => 
+    array (
+      'type' => 'slug',
+      'label' => '슬러그',
+      'width' => '',
+      'width_unit' => '%',
+      'export' => 'off',
+      'sort' => 'on',
+      'edit' => 'off',
+      'bulk_edit' => 'on',
+    ),
+    'posts' => 
+    array (
+      'type' => 'posts',
+      'label' => '카운트',
+      'width' => '',
+      'width_unit' => '%',
+      'export' => 'off',
+      'sort' => 'on',
+    ),
+  ),
+  'settings' => 
+  array (
+    'hide_inline_edit' => 'off',
+    'hide_bulk_edit' => 'off',
+    'hide_filters' => 'off',
+    'hide_smart_filters' => 'off',
+    'hide_segments' => 'off',
+    'hide_export' => 'off',
+    'hide_search' => 'off',
+    'hide_bulk_actions' => 'off',
+    'horizontal_scrolling' => 'off',
+    'sorting' => '0',
+    'sorting_order' => 'asc',
+  ),
+);
