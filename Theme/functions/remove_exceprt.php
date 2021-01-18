18504e993b203f5dc2371c9844e95110e44a09d8
Hansanghyeon <dandan9509@gmail.com>
2021-01-18 19:00:13 +0900

    요약글에 html 태그 제거

--- Theme/functions/remove_exceprt.php
+++ Theme/functions/remove_exceprt.php
@@ -1,11 +1,17 @@
 <?php
 
 /**
- *  * 요약글에 <p> 태그 제거
- *   *
- *    * @author       Hansanghyeon
- *     * @copyright    Hansanghyeon <dandan9509@gmail.com>
- *      **/
+ * 요약글에 <p> 태그 제거
+ *
+ * @author       Hansanghyeon
+ * @copyright    Hansanghyeon <dandan9509@gmail.com>
+ */
 
-remove_filter ('the_exceprt', 'wpautop');
+function wpsites_disable_self_pingbacks(&$links)
+{
+  foreach ($links as $l => $link)
+    if (0 === strpos($link, get_option('home')))
+      unset($links[$l]);
+}
 
+add_action('pre_ping', 'wpsites_disable_self_pingbacks');
