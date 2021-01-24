45bec88fb45644ae805f8cd0cdaf516da2850156
Hansanghyeon <999@hyeon.pro>
2021-01-24 15:56:23 +0900

    :construction: GNB & MSB 연관성 제거

--- src/components/modules/GNB/index.tsx
+++ src/components/modules/GNB/index.tsx
@@ -2,17 +2,13 @@ import Headroom from 'react-headroom';
 // components
 import { SmLogo } from '@atom/Logo';
 import SnsIcon from '@module/list/SNS';
-import MenuToggle from '@atom/toggle/Menu';
 import { Wrap, Col, Row, Container } from './style';
 
 const GNB = () => (
   <Headroom>
-    <Wrap sidebarOpen={false}>
+    <Wrap>
       <Container.Inner fluid>
         <Row>
-          <Col.Menu col={2}>
-            <MenuToggle isActive={false} />
-          </Col.Menu>
           <Col.Left auto>
             <SmLogo />
           </Col.Left>
