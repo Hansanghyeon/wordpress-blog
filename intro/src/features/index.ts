11332349469f5a93e73df4e0f0a8f935f3e42fb2
Hansanghyeon <999@hyeon.pro>
2021-04-05 17:32:59 +0900

    :fire: React intro App

--- intro/src/features/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import { configureStore } from '@reduxjs/toolkit'
-
-const rootReducer = combineReducers({})
-
-const store = configureStore({ reducer: rootReducer })
-
-export type RootState = ReturnType<typeof rootReducer>
-export default store
