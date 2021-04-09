f5a5ede8deb3f771d48f8e4d7220337337358655
Hansanghyeon <999@hyeon.pro>
2021-04-09 12:00:12 +0900

    :sparkles: barba 적용 완료

--- sage/resources/assets/scripts/routes/common.js
+++ sage/resources/assets/scripts/routes/common.js
@@ -1,6 +1,33 @@
+import barba from '@barba/core';
+import gsap from 'gsap';
+
 export default {
   init() {
     // JavaScript to be fired on all pages
+    barba.init({
+      transitions: [
+        {
+          name: 'basic',
+          leave: function (data) {
+            gsap.to(data.current.container, 1, {
+              opacity: 0,
+              onComplete: this.async(),
+            });
+          },
+          enter: function (data) {
+            // Remove the old container
+            const parent = data.current.container.parentNode;
+            parent.removeChild(data.current.container);
+
+            data.current.container.remove();
+            gsap.from(data.next.container, 1, {
+              opacity: 0,
+              onComplete: this.async(),
+            });
+          },
+        },
+      ],
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
