e98582a784eaf9c8017bf444876c0389b4f30432
Hansanghyeon <999@hyeon.pro>
2021-05-12 14:59:43 +0900

    :lipstick: google fonts 노토산스, 독토 폰트

--- sage/resources/views/partials/head.blade.php
+++ sage/resources/views/partials/head.blade.php
@@ -3,6 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Antonio:wght@100;200;300;400;500;600;700&family=Orelega+One&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=East+Sea+Dokdo&display=swap" rel="stylesheet">
   @php wp_head() @endphp
 </head>
