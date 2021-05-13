d52f830ea1dd224a3aca2fe81357c7d2c322aa66
Hansanghyeon <999@hyeon.pro>
2021-05-13 18:26:49 +0900

    :sparkles: toc 모바일 버튼 & 영역 설정 & 스타일

--- sage/resources/views/partials/atom/toc.blade.php
+++ sage/resources/views/partials/atom/toc.blade.php
@@ -1,8 +1,10 @@
 <div class="toc-area">
-  @php 
-  dynamic_sidebar('content_first');
-  @endphp
-  <div class="tocWrap">
+  <div id="toc" class="toc-area-inner">
+    @php 
+    dynamic_sidebar('content_first');
+    @endphp
+  </div>
+  <div class="toc-area-toggle">
     <button id="toggle-toc" class='btn-toc'>
       <img src="/wp-content/uploads/005-archive.svg" alt="">
     </button>
