3d432b0e7d10263f5941818aa43ca458287b5c56
Hansanghyeon <999@hyeon.pro>
2021-05-13 11:41:02 +0900

    :truck: toc 부분만 분리

---
+++ sage/resources/views/partials/atom/toc.blade.php
@@ -0,0 +1,10 @@
+<div class="toc-area">
+  @php 
+  dynamic_sidebar('content_first');
+  @endphp
+  <div class="tocWrap">
+    <button id="toggle-toc" class='btn-toc'>
+      <img src="/wp-content/uploads/005-archive.svg" alt="">
+    </button>
+  </div>
+</div>
