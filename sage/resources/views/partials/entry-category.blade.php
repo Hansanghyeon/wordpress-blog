71bd56d2af45c5df2ef02bcb7f1f13d6fa08f155
Hansanghyeon <999@hyeon.pro>
2021-05-15 18:04:05 +0900

    :sparkles: ditto 컴포넌트 추가

---
+++ sage/resources/views/partials/entry-category.blade.php
@@ -0,0 +1,18 @@
+<ul class="category">
+  @foreach (wp_get_post_terms(get_the_ID(), get_post_type().'_category') as $term)
+    @php
+    $icon = get_field('icon', get_post_type().'_category_'.$term->term_id);
+    if (!is_array($icon)) $icon = acf_get_attachment($icon);
+    $iconUrl = $icon['url'];
+    $iconTItle = $icon['title'];
+    @endphp
+    <li>
+      <a href="$term->slug" class="iconList">
+        <div class="imgWrap">
+          <img src="{{$iconUrl}}" alt="{{$iconTitle}}">
+        </div>
+        <span>{{$term->name}}</span>
+      </a>
+    </li>
+  @endforeach
+</ul>
