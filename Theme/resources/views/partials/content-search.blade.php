0e49836c3630b0d5b148ade963ccb7b804461540
Hansanghyeon <999@hyeon.pro>
2021-05-23 19:13:21 +0900

    :sparkles: search template page

--- Theme/resources/views/partials/content-search.blade.php
+++ Theme/resources/views/partials/content-search.blade.php
@@ -1,11 +1,24 @@
-<article @php post_class() @endphp>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-    @if (get_post_type() === 'post')
-      @include('partials/entry-meta')
-    @endif
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
-  </div>
-</article>
+@php
+$isThumb = !empty(get_the_post_thumbnail());
+if (!empty(get_the_post_thumbnail())) $isThumb = 'data-isthumb';
+@endphp
+<div class="ditto">
+  <article @php post_class() @endphp>
+    <div class="ditto-main" @php echo $isThumb @endphp>
+      <div class="ditto-thumbnail">
+        @php echo get_the_post_thumbnail() @endphp
+      </div>
+      <div class="ditto-main-inner" @php echo $isThumb @endphp>
+        <div class="ditto-body">
+          <h2 class="ditto-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
+          <div class="ditto-content">
+            @php the_excerpt() @endphp
+          </div>
+        </div>
+        <div class="ditto-footer">
+          @include('partials/entry-simple-meta')
+        </div>
+      </div>
+    </div>
+  </article>
+</div>
