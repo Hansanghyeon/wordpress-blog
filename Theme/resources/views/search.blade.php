c7c65aca89963ec02960191b44f888b87f40a121
999hyeon <999@hyeon.pro>
2021-05-23 19:14:48 +0900

    :sparkles: 검색 페이지 디자인 (#640)

--- Theme/resources/views/search.blade.php
+++ Theme/resources/views/search.blade.php
@@ -1,18 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
+  <div class="container">
+    @include('partials.page-header')
+  </div>
+  <div class="container">
+    @if (!have_posts())
+      <div class="alert alert-warning">
+        {{ __('Sorry, no results were found.', 'sage') }}
+      </div>
+      {!! get_search_form(false) !!}
+    @endif
+    
+    <div class="ditto-root">
+      @while (have_posts()) @php the_post() @endphp
+        @include('partials.content-search')
+      @endwhile
     </div>
-    {!! get_search_form(false) !!}
-  @endif
+  </div>
 
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-search')
-  @endwhile
-
-  {!! get_the_posts_navigation() !!}
+  @empty(!$pagination = get_the_posts_pagination( array(
+    'mid_size' => 2,
+    'prev_text' => __( '이전' ),
+    'next_text' => __( '다음' ),
+  )))
+    <div class="container">
+      <div class="page-pagination">
+        {!! $pagination !!}
+      </div>
+    </div>
+  @endempty
 @endsection
