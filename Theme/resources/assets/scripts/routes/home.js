9455a3b4ac75e5d53ade7f39d1775087398994b6
Hansanghyeon <999@hyeon.pro>
2021-05-29 23:14:26 +0900

    :sparkles: kakao 공유 링크 추가 완료

--- Theme/resources/assets/scripts/routes/home.js
+++ Theme/resources/assets/scripts/routes/home.js
@@ -1,11 +1,8 @@
-import loader from '../components/loader';
-
 export default {
   init() {
     // JavaScript to be fired on the home page
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
-    loader();
   },
 };
