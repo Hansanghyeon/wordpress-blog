4db6f1e01897106a5f6a5febdecc01ca54b9af8a
999hyeon <999@hyeon.pro>
2021-07-11 20:07:19 +0900

    :construction: route js

--- DEV/resources/scripts/app.js
+++ DEV/resources/scripts/app.js
@@ -3,6 +3,20 @@
  */
 import 'jquery';
 
-$(() => {
-  // console.log('Hello world');
+// Import everything from autoload
+// import './autoload/**/*';
+
+// import local dependencies
+import Router from './util/Router';
+import common from './routes/common';
+
+/** Populate Router instance with DOM routes */
+const routes = new Router({
+  // All pages
+  common,
+});
+
+// Load Events
+jQuery(document).ready(() => {
+  routes.loadEvents();
 });
