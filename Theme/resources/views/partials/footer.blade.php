1da81612ccdecbfad49ac2d366a0a995e16e5498
999hyeon <999@hyeon.pro>
2021-08-01 15:05:10 +0900

    :sparkles: footer 부분에 방문자 카운터 추가 & 로고 및 sns 제거

--- Theme/resources/views/partials/footer.blade.php
+++ Theme/resources/views/partials/footer.blade.php
@@ -1,19 +1,12 @@
 <footer id="global" class="content-info global">
   <div class="container lg:max-w-screen-lg mx-auto grid grid-flow-row auto-rows-max gap-3">
-    <div class="flex justify-center">
-      <a class="logo icon" href="{{ home_url('/') }}">
-        <img src="{{ get_site_icon_url() }}" alt="{{ get_bloginfo('name', 'display') }}">
-      </a>
-    </div>
+    <x-VisitorCounter />
     <a href="/history">
       Since 2014.7.22 - <span id="history"></span>
     </a>
     <span>
       Copyright (c) 2021 999hyeon
     </span>
-    <div class="flex justify-center mb-3">
-      @include('partials.sns')
-    </div>
     @php dynamic_sidebar('sidebar-footer') @endphp
   </div>
 </footer>
