02ae3374f3409912b05ee765f99d611655f17c5e
999hyeon <999@hyeon.pro>
2021-10-08 15:30:07 +0900

    :fire: admin custom css disable

--- Theme/wp-functions/admin/dashboard/index.php
+++ Theme/wp-functions/admin/dashboard/index.php
@@ -11,11 +11,11 @@ define('ADMIN_DIR', str_replace("/var/www/html", "", plugin_dir_path(__FILE__)))
 
 function admin_style()
 {
-  wp_enqueue_style('WP_list_table', ADMIN_DIR . 'style.css');
-  wp_enqueue_style('admin_toolbar', ADMIN_DIR . 'admin-toolbar.css');
-  wp_enqueue_style('wp_block', ADMIN_DIR . 'wp_block.css');
-  wp_enqueue_style('dark_mode_100', ADMIN_DIR . 'dark_mode.css');
-  wp_enqueue_style('ac-work', ADMIN_DIR . 'work.css');
-  wp_enqueue_style('FontAwesome', 'https://use.fontawesome.com/releases/v5.6.3/css/all.css', false);
+    wp_enqueue_style('WP_list_table', ADMIN_DIR . 'style.css');
+    wp_enqueue_style('admin_toolbar', ADMIN_DIR . 'admin-toolbar.css');
+    wp_enqueue_style('wp_block', ADMIN_DIR . 'wp_block.css');
+    wp_enqueue_style('dark_mode_100', ADMIN_DIR . 'dark_mode.css');
+    wp_enqueue_style('ac-work', ADMIN_DIR . 'work.css');
+    wp_enqueue_style('FontAwesome', 'https://use.fontawesome.com/releases/v5.6.3/css/all.css', false);
 }
-add_action('admin_enqueue_scripts', 'admin_style');
+// add_action('admin_enqueue_scripts', 'admin_style');
