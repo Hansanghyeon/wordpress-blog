90a3fc17d8a695f585f4e9e1b86ee3957373c2f5
Hansanghyeon <999@hyeon.pro>
2021-11-18 14:16:24 +0900

    :lipstick: github style 반영

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -12,6 +12,32 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        'shuttle-gray': {
+          50: '#f7f8f8',
+          100: '#eff1f2',
+          200: '#d8dbde',
+          300: '#c1c5ca',
+          400: '#929aa3',
+          500: '#636e7b',
+          600: '#59636f',
+          700: '#4a535c',
+          800: '#3b424a',
+          900: '#31363c',
+        },
+        bombay: {
+          50: '#fbfbfc',
+          100: '#f7f8f9',
+          200: '#ebedf0',
+          300: '#dfe3e6',
+          400: '#c7cdd4',
+          500: '#afb8c1',
+          600: '#9ea6ae',
+          700: '#838a91',
+          800: '#696e74',
+          900: '#565a5f',
+        },
+      },
       typography: {
         DEFAULT: {
           css: {
