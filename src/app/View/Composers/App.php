a80ac0df079a05f048e0f39939bc92796d035945
Hansanghyeon <999@hyeon.pro>
2021-12-02 13:18:00 +0900

    :sparkles: 캐싱된 페이지에서 js가 재대로 작동하지않아? 얼마나 진행됐는지 보여주는 history가 안보이는 문제 해결

--- src/app/View/Composers/App.php
+++ src/app/View/Composers/App.php
@@ -3,6 +3,7 @@
 namespace App\View\Composers;
 
 use Roots\Acorn\View\Composer;
+use Carbon\Carbon;
 
 class App extends Composer
 {
@@ -24,6 +25,7 @@ class App extends Composer
     {
         return [
             'siteName' => $this->siteName(),
+            'history' => $this->history()
         ];
     }
 
@@ -35,5 +37,17 @@ class App extends Composer
     public function siteName()
     {
         return get_bloginfo('name', 'display');
+    }
+
+    public function history()
+    {
+      $dtCreateblog = Carbon::create(2014, 7, 22, 0, 0, 0, 'Asia/Seoul');
+      $dtNow = Carbon::now('Asia/Seoul');
+
+      $days = $dtNow->diffInDays($dtCreateblog);
+      $month = $dtNow->diffInMonths($dtCreateblog) % 12;
+      $year = $dtNow->diffInYears($dtCreateblog);
+
+      return "{$year}년 {$month}개월째 ({$days}일)";
     }
 }
--- src/resources/views/partials/footer.blade.php
+++ src/resources/views/partials/footer.blade.php
@@ -1,9 +1,11 @@
 <footer id="global" class="content-info global">
   <div class="container lg:max-w-screen-lg mx-auto grid grid-flow-row auto-rows-max gap-3">
     <x-VisitorCounter />
-    <a href="/history">
-      Since 2014.7.22 - <span id="history"></span>
-    </a>
+    <div class="text-center">
+      <a href="/history">
+        Since 2014.7.22 - <span id="history">{!! $history !!}</span>
+      </a>
+    </div>
     <span>
       Copyright (c) 2021 999hyeon
     </span>
