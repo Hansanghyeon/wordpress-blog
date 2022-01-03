d13165149cd484a9bb43258c0d74b0a2a006f8a7
Hansanghyeon <999@hyeon.pro>
2022-01-03 18:09:48 +0900

    :construction: 임시 포트폴리오 싱글페이지 완료

---
+++ src/resources/views/partials/content-single-portfolio.blade.php
@@ -0,0 +1,9 @@
+<article @php(post_class())>
+  @include('partials/header-portfolio')
+  <div class="entry-content">
+    @include('partials.toc')
+    <div class="prose">
+      @php(the_content())
+    </div>
+  </div>
+</article>
