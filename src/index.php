78c1755ca7b518c8e7e17148b99fd49d86005572
Hansanghyeon <999@hyeon.pro>
2022-01-03 18:22:07 +0900

    :bug: d2coding 윈도우에서 폰트 작동하지 않음

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="ko_KR">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Google Fonts -->
@@ -8,22 +9,26 @@
     <link href="https://fonts.googleapis.com/css2?family=East+Sea+Dokdo&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <!-- D2Codnig -->
-    <link href="https://cdn.jsdelivr.net/gh/joungkyun/font-d2coding/d2coding.css" rel="stylesheet">
+    <link href="//cdn.jsdelivr.net/gh/wan2land/d2coding/d2coding-full.css" rel="stylesheet" />
+    <link href="//cdn.jsdelivr.net/gh/wan2land/d2coding/d2coding-subset.css" rel="stylesheet" />
+    <link href="//cdn.jsdelivr.net/gh/wan2land/d2coding/d2coding-ligature-full.css" rel="stylesheet" />
+    <link href="//cdn.jsdelivr.net/gh/wan2land/d2coding/d2coding-ligature-subset.css" rel="stylesheet" />
     <!-- Kakao -->
     <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
     <!-- Hansanghyeon Font -->
     <link rel="stylesheet" type="text/css" href="https://hansanghyeon.github.io/Arita/font.min.css">
 
     <?php wp_head(); ?>
-  </head>
+</head>
 
-  <body <?php body_class(); ?> x-data>
+<body <?php body_class(); ?> x-data>
     <?php wp_body_open(); ?>
     <?php do_action('get_header'); ?>
 
     <?php echo \Roots\view(\Roots\app('sage.view'), \Roots\app('sage.data'))->render(); ?>
-    
+
     <?php do_action('get_footer'); ?>
     <?php wp_footer(); ?>
-  </body>
+</body>
+
 </html>
