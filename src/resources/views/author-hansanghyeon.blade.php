6df8dd3b29ed5729f0883a7ff98be57fb4499b17
Hansanghyeon <999@hyeon.pro>
2022-01-04 18:28:53 +0900

    :sparkles: 경력표시 워드프레스 컨텐츠로 마이그레이션

--- src/resources/views/author-hansanghyeon.blade.php
+++ src/resources/views/author-hansanghyeon.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="container max-w-screen-md">
-    <div class="page-header">
+    <div class="page-header pb-0">
       @include('partials.resume.profile')
     </div>
     <div class="prose max-w-none">
