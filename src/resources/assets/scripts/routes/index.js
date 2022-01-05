d11220964fabcdd774da5dc610bad7c90e9043f4
Hansanghyeon <999@hyeon.pro>
2022-01-05 12:56:35 +0900

    :construction: portfolio route 스크립트 추가

--- src/resources/assets/scripts/routes/index.js
+++ src/resources/assets/scripts/routes/index.js
@@ -3,12 +3,14 @@ import singleDev from './single-dev';
 import singleBlog from './single-blog';
 import taxonomy from './taxonomy';
 import history from './history';
+import singlePortfolio from './single-portfolio';
 
 export default {
   // All pages
   common,
   singleDev,
   singleBlog,
+  singlePortfolio,
   taxDevCategory: taxonomy,
   taxDevTag: taxonomy,
   taxBlogCategory: taxonomy,
