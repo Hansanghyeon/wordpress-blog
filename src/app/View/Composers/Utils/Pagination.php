89abd17e89b3ba17214ef1fdfed48692ee67d22f
Hansanghyeon <999@hyeon.pro>
2022-01-30 16:33:12 +0900

    :art: tab size 4

--- src/app/View/Composers/Utils/Pagination.php
+++ src/app/View/Composers/Utils/Pagination.php
@@ -6,37 +6,37 @@ use function Roots\view;
 
 class Pagination
 {
-  public function render($range = 2)
-  {
-    $showitems = ($range * 2) + 1;
+    public function render($range = 2)
+    {
+        $showitems = ($range * 2) + 1;
 
-    global $paged;
-    if (empty($paged)) {
-        $paged = 1;
+        global $paged;
+        if (empty($paged)) {
+            $paged = 1;
+        }
+
+        global $wp_query;
+        $pages = $wp_query->max_num_pages;
+        if (!$pages) {
+            $pages = 1;
+        }
+
+        if ($pages > 1) {
+            $prevlink = $paged > 1 && $showitems < $pages ? get_pagenum_link($paged - 1) : '';
+            $nextlink = $paged < $pages && $showitems < $pages ? get_pagenum_link($paged + 1) : '';
+            $lastlink = get_pagenum_link($pages);
+            $of = $paged + $showitems;
+
+            return view('partials.pagination', [
+                'pages' => $pages,
+                'range' => $range,
+                'of' => $of,
+                'paged' => $paged,
+                'prevlink' => $prevlink,
+                'nextlink' => $nextlink,
+                'lastlink' => $lastlink,
+                'showitems' => $showitems,
+            ]);
+        }
     }
-
-    global $wp_query;
-    $pages = $wp_query->max_num_pages;
-    if (!$pages) {
-        $pages = 1;
-    }
-
-    if ($pages > 1) {
-        $prevlink = $paged > 1 && $showitems < $pages ? get_pagenum_link($paged - 1) : '';
-        $nextlink = $paged < $pages && $showitems < $pages ? get_pagenum_link($paged + 1) : '';
-        $lastlink = get_pagenum_link($pages);
-        $of = $paged + $showitems;
-
-        return view('partials.pagination', [
-          'pages' => $pages,
-          'range' => $range,
-          'of' => $of,
-          'paged' => $paged,
-          'prevlink' => $prevlink,
-          'nextlink' => $nextlink,
-          'lastlink' => $lastlink,
-          'showitems' => $showitems,
-        ]);
-    }
-  }
 }
