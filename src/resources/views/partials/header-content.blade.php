abdd3b3ebb6a4a2f5c51cd2f508ddc56734a3474
Hansanghyeon <999@hyeon.pro>
2022-02-01 09:11:56 +0900

    :bug: 포스트 taxonomy 로직변경 태그 미반영 오류 수정

--- src/resources/views/partials/header-content.blade.php
+++ src/resources/views/partials/header-content.blade.php
@@ -12,7 +12,7 @@
     <ul class="flex gap-1 mt-2 mb-2 flex-wrap">
       @foreach ($tags as $term)
         <li>
-          <a href="{!! $term['link'] !!}" class="flex items-center">
+          <a href="{!! $term->permalink !!}" class="flex items-center">
             <svg viewBox="0 0 464 464" width="0.8em" height="1em">
               <path style="fill:currentColor" d="M436,128h-56.504l19.744-85.056c3.504-15.096-5.44-30.288-20.344-34.544
               c-15.992-4.568-32.56,5.184-36.32,21.384L319.784,128H188.992l19.744-85.056c3.504-15.096-5.44-30.288-20.344-34.544
@@ -22,7 +22,7 @@
               c15.992,4.568,32.56-5.184,36.32-21.392L331.208,336H396c15.464,0,28-12.536,28-28s-12.536-28-28-28h-51.792l22.288-96H436
               c15.464,0,28-12.536,28-28S451.464,128,436,128z M284.496,280H153.704l22.288-96h130.792L284.496,280z"/>
             </svg>
-            <span class="font-semibold">{{$term['name']}}</span>
+            <span class="font-semibold">{!! $term->name !!}</span>
           </a>
         </li>
       @endforeach
