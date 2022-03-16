d53eba45c715ff122dcd8ae62792a0b3a2baeb59
Hansanghyeon <999@hyeon.pro>
2022-03-17 08:48:55 +0900

    fix: VisitorCounter 구글 api 키이름 변경

--- src/app/View/Components/VisitorCounter.php
+++ src/app/View/Components/VisitorCounter.php
@@ -36,7 +36,7 @@ class VisitorCounter extends Component
 
     public function initializeAnalytics()
     {
-        $KEY_FILE_LOCATION = get_theme_file_path('/key/log-278509-f525776d2ba6.json');
+        $KEY_FILE_LOCATION = get_theme_file_path('/key/log-278509-7de64d610f0f.json');
 
         // Create and configure a new client object.
         $client = new Google_Client();
