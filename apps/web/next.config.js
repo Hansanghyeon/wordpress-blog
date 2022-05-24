2418486907d6b20fc9171c1932c81b189b2dd53c
Hansanghyeon <999@hyeon.pro>
2022-05-24 18:51:56 +0900

    feat: init faust.js wpheadless framework 🚀

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,10 @@
+const { withFaust } = require("@faustjs/next");
 const withTM = require("next-transpile-modules")(["ui"]);
 
+/**
+ * @type {import('next').NextConfig}
+ **/
+module.exports = withFaust();
 module.exports = withTM({
   reactStrictMode: true,
 });
--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,7 +1,18 @@
+import "~web/faust.config";
+import { FaustProvider } from "@faustjs/next";
+import "normalize.css/normalize.css";
+import React from "react";
+import "~web/scss/main.scss";
+import { client } from "~web/client";
 import type { AppProps } from "next/app";
-import "~web/styles/style.scss";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  // @ts-ignore
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <FaustProvider client={client} pageProps={pageProps}>
+        {/* @ts-ignore */}
+        <Component {...pageProps} />
+      </FaustProvider>
+    </>
+  );
 }
