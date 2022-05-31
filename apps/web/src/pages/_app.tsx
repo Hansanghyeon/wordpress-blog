1cdeb94c6d592e39a4c041a85dcd7b4e53177271
Hansanghyeon <999@hyeon.pro>
2022-05-31 12:38:45 +0900

    feat: default font Pretendard

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import "~web/faust.config";
 import { FaustProvider } from "@faustjs/next";
-import "normalize.css/normalize.css";
-import React from "react";
-import "~web/scss/main.scss";
 import { client } from "~web/client";
 import type { AppProps } from "next/app";
+import "normalize.css/normalize.css";
+import "~web/scss/main.scss";
+import "pretendard/dist/web/static/pretendard.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
--- packages/tailwindcss-config/tailwind.config.js
+++ packages/tailwindcss-config/tailwind.config.js
@@ -1,12 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "../../packages/ui/components/**/*.{tsx,scss}",
-    "../../app/web/src/**/*.{tsx,scss}"
+    "../../app/web/src/**/*.{tsx,scss}",
   ],
   theme: {
     extend: {
       container: {
         center: true,
+      },
+      fontFamily: {
+        sans: ['"Pretendard"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
