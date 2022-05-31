a9b745dad97d6e7120b4cced2858e09662d9fa1f
Hansanghyeon <999@hyeon.pro>
2022-05-31 20:03:36 +0900

    build(deps): unicons & scss import

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -4,8 +4,12 @@ import { FaustProvider } from "@faustjs/next";
 import { client } from "~web/client";
 import type { AppProps } from "next/app";
 import "normalize.css/normalize.css";
-import "~web/scss/main.scss";
 import "pretendard/dist/web/static/pretendard.css";
+import "d2coding/d2coding-full.css";
+import "d2coding/d2coding-subset.css";
+import "d2coding/d2coding-ligature-full.css";
+import "d2coding/d2coding-ligature-subset.css";
+import "~web/scss/app.scss";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
