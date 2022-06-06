be7425f7192fa0acc0cfd4a6c3098b7ed7be6617
Hansanghyeon <999@hyeon.pro>
2022-06-07 07:12:06 +0900

    feat: home page 마이그레이션 완료

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,181 +1,83 @@
 import { getNextStaticProps } from "@faustjs/next";
-
 import { GetStaticPropsContext } from "next";
+import Link from "next/link";
 import Head from "next/head";
 import React from "react";
-import { CTA, Footer, Header, Hero, Posts } from "~web/components";
-import styles from "~web/scss/pages/home.module.scss";
-import { client } from "~web/client";
-
-import { Button } from "ui";
+import { client, PageIdType, MenuNodeIdTypeEnum } from "~web/client";
+import { WPGBlocks } from "~web/lib";
 
 export default function Page() {
-  const { usePosts, useQuery } = client;
+  const { useQuery, usePage } = client;
   const generalSettings = useQuery().generalSettings;
-  const posts = usePosts({
-    first: 6,
-    where: {
-      categoryName: "uncategorized",
-    },
+
+  const page = usePage({
+    id: "18",
+    idType: PageIdType.DATABASE_ID,
   });
+
+  const menuItems = useQuery().menuItems();
+
+  console.log(menuItems);
 
   return (
     <>
-      <Header
-        title={generalSettings.title}
-        description={generalSettings.description}
-      />
-
       <Head>
         <title>
           {generalSettings.title} - {generalSettings.description}
         </title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="https://fonts.gstatic.com"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=East+Sea+Dokdo&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <main className="content bg-blue-500">
-        <Button />
-        <Hero
-          title="Get Started with Faust.js"
-          buttonText="Developer Docs"
-          buttonURL="https://faustjs.org"
-          button2Text="Faust.js on GitHub"
-          button2URL="https://github.com/wpengine/faustjs"
-          bgImage="/images/headless_hero_background.jpg"
-          id={styles.home_hero}
-        >
-          <p>
-            WP&nbsp;Engine’s Faust.js Framework includes this example project,
-            the{" "}
-            <a href="https://wordpress.org/plugins/faustwp/">FaustWP plugin</a>,{" "}
-            <a href="https://github.com/wpengine/faustjs">headless packages</a>,
-            and{" "}
-            <a href="https://faustjs.org/docs/tutorial/dev-env-setup">
-              tutorials
-            </a>{" "}
-            to make building headless WordPress sites fast and fun.
-          </p>
-        </Hero>
-        <section className={styles.explore}>
-          <div className="wrap">
-            <h2>Explore this Example Project</h2>
-            <p>
-              This headless example project uses{" "}
-              <a href="https://nextjs.org/">Next.js</a>,{" "}
-              <a href="https://graphql.org/">GraphQL</a>,{" "}
-              <a href="https://gqty.dev">GQty</a> and the{" "}
-              <a href="https://github.com/wpengine/faustjs">
-                WP&nbsp;Engine headless packages
-              </a>{" "}
-              for WordPress integration. Dive in and edit this template at{" "}
-              <code>src/pages/index.tsx</code> or discover more below.
-            </p>
-            <div className={styles.features}>
-              <div className={styles.feature}>
-                <h3>Static Site Generation</h3>
-                <p>
-                  This example project uses Next.js&apos;{" "}
-                  <a href="https://nextjs.org/docs/basic-features/pages#two-forms-of-pre-rendering">
-                    Static Site Generation (SSR)
-                  </a>
-                  . SSG unlocks better performance by statically generating
-                  pages that can then be cached by a CDN.
-                </p>
-              </div>
-
-              <div className={styles.feature}>
-                <h3>Incremental Static Regeneration</h3>
-                <p>
-                  This example project uses Next.js&apos;{" "}
-                  <a href="https://vercel.com/docs/concepts/next.js/incremental-static-regeneration">
-                    Incremental Static Regeneration (ISR)
-                  </a>
-                  . This allows you to revalidate static pages without having to
-                  rebuild your entire site. By default, Faust.js sets a{" "}
-                  <strong>
-                    15 minute <code>revalidate</code> time per page.
-                  </strong>{" "}
-                  <br />
-                  <a href="https://faustjs.org/docs/next/guides/ssr-ssg#setting-up-incremental-static-regeneration-isr">
-                    Learn more about how Faust.js uses ISR.
-                  </a>
-                </p>
-              </div>
-
-              <div className={styles.feature}>
-                <h3>Ready for Atlas</h3>
-                <p>
-                  <a href="https://wpengine.com/atlas">Atlas</a> is the complete
-                  Headless WordPress Platform for absurdly fast dynamic sites.
-                  One headless solution, one price, all in one place.
-                  <br />
-                </p>
-              </div>
-            </div>
-            <div className={styles.features}>
-              <div className={styles.feature}>
-                <h3>Global Styles and Fonts</h3>
-                <p>
-                  Add styles to load on every page, such as typography and
-                  layout rules, in <code>src/scss/main.scss</code>. The project
-                  adds{" "}
-                  <a href="https://necolas.github.io/normalize.css/">
-                    normalize.css
-                  </a>{" "}
-                  in <code>src/pages/_app.tsx</code>. Google Fonts are enqueued
-                  in <code>src/pages/_document.tsx</code>.
-                </p>
-              </div>
-
-              <div className={styles.feature}>
-                <h3>Hooks</h3>
-                <p>
-                  Fetch data from WordPress with <code>usePost</code>,{" "}
-                  <code>usePosts</code>, <code>useGeneralSettings</code> and
-                  other custom hooks. Use these hooks in your page templates to
-                  pass data to custom components. See{" "}
-                  <code>src/pages/index.tsx</code> for examples.
-                </p>
-              </div>
-
-              <div className={styles.feature}>
-                <h3>Components</h3>
-                <p>
-                  Add or edit components in the <code>src/components</code>{" "}
-                  folder. Find component styles at{" "}
-                  <code>src/scss/components</code>, which use{" "}
-                  <a href="https://nextjs.org/docs/basic-features/built-in-css-support#adding-component-level-css">
-                    CSS modules
-                  </a>{" "}
-                  to scope CSS to each component.
-                </p>
-              </div>
-            </div>
+      <div className="container pt-12 lg:max-w-screen-lg">
+        <div className="font-dokdo mb-[45px] text-[48px] font-normal leading-[1]">
+          <WPGBlocks blocks={page?.blocks ?? ""} />
+        </div>
+        <div className="w-full md:w-3/6 lg:w-full">
+          <div className="content prose-lg">
+            <h2 className="font-dokdo mb-4">#부캐</h2>
           </div>
-        </section>
-        <Posts
-          posts={posts.nodes}
-          heading="Latest Posts"
-          intro="The Posts component in src/pages/index.tsx shows the latest six posts from the connected WordPress site."
-          headingLevel="h2"
-          postTitleLevel="h3"
-          id={styles.post_list}
-        />
-        <CTA
-          title="Questions or comments?"
-          buttonText="Join the discussion on GitHub"
-          buttonURL="https://github.com/wpengine/faustjs/discussions"
-          headingLevel="h2"
-        >
-          <p>
-            We welcome feature requests, bug reports and questions in the{" "}
-            <a href="https://github.com/wpengine/faustjs">
-              Headless Framework GitHub repository
-            </a>
-            .
-          </p>
-        </CTA>
-      </main>
-      <Footer copyrightHolder={generalSettings.title} />
+          <ul className="flex">
+            {menuItems.nodes.map((menuItem) => {
+              const { uri, thumbnail, label } = menuItem;
+              console.log(uri);
+              return (
+                <li className="p-2">
+                  <Link href={uri}>
+                    <a href={uri}>
+                      <div className="grid max-w-[125px] gap-y-[8px]">
+                        <img
+                          className="icon-card"
+                          src={thumbnail()?.menu_icon}
+                          alt={label}
+                        />
+                        <div className="justify-self-center">
+                          <span
+                            style={{
+                              backgroundColor: thumbnail()?.label_color,
+                            }}
+                            className="rounded-[4px] border-[2px] border-[color:rgba(var(--RGB-gl4),0.125)] p-[0.075rem_0.5rem_0.125rem] text-black"
+                          >
+                            {label}
+                          </span>
+                        </div>
+                      </div>
+                    </a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
