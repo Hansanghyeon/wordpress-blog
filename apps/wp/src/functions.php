047c0b0972f5f5e2bdabb04d27e145cd610d1816
Hansanghyeon <999@hyeon.pro>
2022-06-06 16:26:53 +0900

    feat: raw 타입 공개 쿼리

--- apps/wp/src/functions.php
+++ apps/wp/src/functions.php
@@ -60,3 +60,15 @@ add_filter("acf/format_value", "acf_nullify_empty", 100, 3);
 add_filter('preview_post_link', function () {
   return get_site_url() . '/preview/';
 });
+
+add_action( 'graphql_register_types', function() {
+  register_graphql_field( 'ContentNode', 'blocks', [
+    'type' => 'String',
+    'description' => __( 'Link to edit the content', 'your-textdomain' ),
+    'resolve' => function( \WPGraphQL\Model\Post $post, $args, $context, $info ) {
+      $wPost = get_post($post->ID, null, 'raw');
+      return $wPost->post_content;
+    }
+  ]);
+} );
+
