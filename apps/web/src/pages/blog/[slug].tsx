8e19482b585cbb6e4e029d83e779fc3c928e2394
Hansanghyeon <999@hyeon.pro>
2022-07-20 22:43:43 +0900

    feat: post type slug template

---
+++ apps/web/src/pages/blog/[slug].tsx
@@ -0,0 +1,72 @@
+import { getNextStaticProps, is404 } from "@faustjs/next";
+import { client, Blog, BlogIdType } from "~web/client";
+import { Footer, Header, Hero } from "~web/components";
+import { GetStaticPropsContext } from "next";
+import Head from "next/head";
+import { is404c } from "~web/lib";
+
+export interface PostProps {
+  post: Blog | Blog["preview"]["node"] | null | undefined;
+}
+
+export function DevComponent({ post }: PostProps) {
+  const { useQuery } = client;
+  const generalSettings = useQuery().generalSettings;
+
+  return (
+    <>
+      <Header
+        title={generalSettings.title}
+        description={generalSettings.description}
+      />
+
+      <Head>
+        <title>
+          {post?.title()} - {generalSettings.title}
+        </title>
+      </Head>
+
+      <Hero
+        title={post?.title()}
+        bgImage={post?.featuredImage?.node?.sourceUrl()}
+      />
+
+      <main className="content content-single">
+        <div className="wrap">
+          <div dangerouslySetInnerHTML={{ __html: post?.content() ?? "" }} />
+        </div>
+      </main>
+
+      <Footer copyrightHolder={generalSettings.title} />
+    </>
+  );
+}
+
+export default function Page({ slug }) {
+  const { useQuery } = client;
+  const dev = useQuery().blog({
+    id: slug,
+    idType: BlogIdType.SLUG,
+  });
+
+  return <DevComponent post={dev} />;
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const { slug } = context.params;
+  return getNextStaticProps(context, {
+    Page,
+    client,
+    notFound: await is404c(context, { client }, "blog"),
+    props: {
+      slug,
+    },
+  });
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
