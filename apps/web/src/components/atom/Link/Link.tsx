680f49709d5c07d438ef21e2e9816bb1beab6828
Hansanghyeon <999@hyeon.pro>
2022-07-20 22:43:14 +0900

    feat: Link

--- apps/web/src/components/atom/Link/Link.tsx
+++ apps/web/src/components/atom/Link/Link.tsx
@@ -1,10 +1,10 @@
 import * as _Link from 'next/link';
 
 const Link = (props) => {
-  const { children, href } = props;
+  const { children, href, ...rest } = props;
   if (!href)
-    return <button {...props}>{children}</button>
-  return <_Link.default href={href}><a {...props}>{children}</a></_Link.default>
+    return <button {...rest}>{children}</button>
+  return <_Link.default href={href}><a {...rest}>{children}</a></_Link.default>
 }
 
 export default Link
