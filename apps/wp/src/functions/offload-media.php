ccfad9b67a8b776b86284e9bbd1a667d099c2634
Hansanghyeon <999@hyeon.pro>
2022-07-25 06:34:58 +0900

    feat(wp): use minio

---
+++ apps/wp/src/functions/offload-media.php
@@ -0,0 +1,13 @@
+<?php
+
+function minio_s3_client_args( $args ) {
+  $args['endpoint'] = 'http://minio:9000';
+  $args['use_path_style_endpoint'] = true;
+  return $args;
+}
+add_filter( 'as3cf_aws_s3_client_args', 'minio_s3_client_args' );
+
+add_filter( 'as3cf_aws_s3_url_domain', 'minio_s3_url_domain', 10, 2 );
+function minio_s3_url_domain( $domain, $bucket ) { 
+    return 'bucket.hyeon.pro/' . $bucket;
+}
