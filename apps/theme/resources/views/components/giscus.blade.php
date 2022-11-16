7fca9e2b0922f23fe2ff3029ff2755f0ffb3655f
Hansanghyeon <999@hyeon.pro>
2022-11-16 22:01:54 +0000

    fix: giscus 저장소 주소 오류 수정

--- apps/theme/resources/views/components/giscus.blade.php
+++ apps/theme/resources/views/components/giscus.blade.php
@@ -1,14 +1,5 @@
-
-  <script src="https://giscus.app/client.js"
-  data-repo="Hansanghyeon/Hansanghyeon"
-  data-repo-id="MDEwOlJlcG9zaXRvcnkyMjc4NTg3MjM="
-  data-category="comment"
-  data-category-id="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMzMDM2NDcz"
-  data-mapping="specific"
-  data-term="{!! $id !!}"
-  data-reactions-enabled="1"
-  data-emit-metadata="1"
-  data-theme="preferred_color_scheme"
-  crossorigin="anonymous"
-  async>
-</script>
+<script src="https://giscus.app/client.js" data-repo="Hansanghyeon/hyeon.pro"
+    data-repo-id="MDEwOlJlcG9zaXRvcnkyMjc4NTg3MjM=" data-category="comment"
+    data-category-id="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMzMDM2NDcz" data-mapping="specific"
+    data-term="{!! $id !!}" data-reactions-enabled="1" data-emit-metadata="1"
+    data-theme="preferred_color_scheme" crossorigin="anonymous" async></script>
