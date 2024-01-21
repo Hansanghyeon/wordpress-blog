522a24a3409301778e0f1f9522920b45f418d2dd
Hansanghyeon <999@hyeon.pro>
2024-01-21 07:51:26 +0000

    feat: remove 방문자 카운터

--- theme/resources/views/partials/footer.blade.php
+++ theme/resources/views/partials/footer.blade.php
@@ -1,6 +1,5 @@
 <footer id="global" class="content-info global">
   <div class="container lg:max-w-screen-lg grid grid-flow-row auto-rows-max gap-3">
-    <x-VisitorCounter />
     <div class="text-center">
       <a href="/history">
         Since 2014.7.22 - <span id="history">{!! $history !!}</span>
